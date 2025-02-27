aa0c7ee0aaefeab1435b065bb144efea47048482
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-27 20:00:00 +0100

    Create aula04_Primeiros_Comandos.py

---
+++ 1_primeiros_passos/1_primeiros_Comandos/aula04_Primeiros_Comandos.py
@@ -0,0 +1,39 @@
+print('Olá Mundo!')
+
+print("olá mundo")
+
+print(7+4)
+
+print("7"+"4")
+
+print("Olá", 5)
+
+nome= "Thaís"
+idade= 30
+peso= 56,8
+
+print(nome, idade, peso)
+
+print("o meu nome é: ", nome, "minha idade é: ", idade, "o meu peso é: ", peso)
+
+# nome = input("Qual seu nome?")
+# idade = input("Qual a sua idade?")
+# peso = input ("Qual é o seu peso?")
+# print(nome, idade, peso)
+
+#DESAFIO
+#1
+# nome = input ("Olá, qual o seu nome? ")
+# print("Seja bem vindo(a)",nome,"! Prazer em te conhecer.")
+
+#2
+# dia= input("Digite o dia que você nasceu: ")
+# mes= input("Digite o mês que você nasceu: ")
+# ano= input("digite o ano que você nasceu: ")
+
+# print("Você nasceu no dia", dia, "do mês:", mes, "do ano de:", ano, ".Correto?")
+
+#3
+n1 = input("Digite o numero1:")
+n2= input("digite o número2:")
+print(n1+n2)
