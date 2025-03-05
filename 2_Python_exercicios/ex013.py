dcef003881108fe59f47d243432cf70a9e00496d
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-05 14:42:46 +0100

    Create ex013.py
    - aumento salario em 15%

---
+++ 2_Python_exercicios/ex013.py
@@ -0,0 +1,4 @@
+# salário funcionário, novo salário com 15% aumento
+
+salário = float(input("Digite o valor do seu salário: "))
+print(f"O seu salário atual é: {salário:.2f} e terá um aumento de 15% passando a ser: {salário + (salário * 0.15):.2f}")
