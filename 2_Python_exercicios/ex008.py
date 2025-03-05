d6b43241ecece50daec3cf0ad592af13eaba999b
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-05 13:38:36 +0100

    Update ex008.py

--- 2_Python_exercicios/ex008.py
+++ 2_Python_exercicios/ex008.py
@@ -2,4 +2,4 @@
 # centímetro: 100 centímetros corresponde a 1 metro. milímetro: 1000 milímetros corresponde a 1 metro.
 
 metros = float(input("Digite o valor em metros para fazermos a conversão em centímetros e milímetros: "))
-print(f"{metros} metros convertidos para centímetros são: {metros * 100}. Já {metros} metros convertidos em milímetros são: {metros * 1000}")
+print(f"{metros} metros convertidos para centímetros são: {metros * 100}. \nJá {metros} metros convertidos em milímetros são: {metros * 1000}")
