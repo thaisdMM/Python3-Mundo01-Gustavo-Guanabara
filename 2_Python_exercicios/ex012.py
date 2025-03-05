4f1a75abc886381c836b616c2cd3a8bacc045423
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-05 15:59:48 +0100

    Update ex012.py

--- 2_Python_exercicios/ex012.py
+++ 2_Python_exercicios/ex012.py
@@ -1,4 +1,4 @@
-# ler preço produto, novo preço com 5% desconto
+# ler preço produto, novo preço com 5% desconto - porcentagem pode ser assim preco_produto*5/100
 
-preco_produto = float(input("Digite o preço do produto: "))
-print(f"O valor do produto era {preco_produto}.\nO novo preço do produto com o desconto de 5% é: {preco_produto - (preco_produto * 0.05)}")
+preco_produto = float(input("Digite o preço do produto:R$ "))
+print(f"O valor do produto era {preco_produto:.2f}.\nO novo preço do produto com o desconto de 5% é R$: {preco_produto - (preco_produto * 0.05):.2f}")
