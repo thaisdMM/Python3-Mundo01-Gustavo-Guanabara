2fe483a249edb06c4bcbd9295692e613c2a78447
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-05 15:51:13 +0100

    Update ex010.py

--- 2_Python_exercicios/ex010.py
+++ 2_Python_exercicios/ex010.py
@@ -1,4 +1,4 @@
 # quanto dinheiro a pessoa tem na carteira e quantos dolares ela pode comprar - dolar na aula 3,27
 
-dinheiro_carteira = float(input("Informe quanto dinheiro você tem na sua carteira: "))
-print(f"A conversão de {dinheiro_carteira} reais para dólares com a cotação de 3,27 é: {dinheiro_carteira / 3.27:.3f} dólares")
+dinheiro_carteira = float(input("Informe quanto dinheiro você tem na sua carteira R$: "))
+print(f"A conversão de {dinheiro_carteira:.2f} reais para dólares com a cotação de 3,27 é: {dinheiro_carteira / 3.27:.2f} US$ ")
