d935d320a69cd1b4c12078e67c90c9aa3eb66487
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-05 14:37:55 +0100

    Create ex012.py
    - produto com desconto de 5%

---
+++ 2_Python_exercicios/ex012.py
@@ -0,0 +1,4 @@
+# ler preço produto, novo preço com 5% desconto
+
+preco_produto = float(input("Digite o preço do produto: "))
+print(f"O valor do produto era {preco_produto}.\nO novo preço do produto com o desconto de 5% é: {preco_produto - (preco_produto * 0.05)}")
