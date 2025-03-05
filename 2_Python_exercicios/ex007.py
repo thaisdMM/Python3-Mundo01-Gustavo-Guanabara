bc83d116908b110e97e8111eb8ae450f11c98578
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-05 12:10:02 +0100

    Create ex007.py

---
+++ 2_Python_exercicios/ex007.py
@@ -0,0 +1,7 @@
+# ler duas notas de aluno, calcule e mostre sua média
+
+nota1 = int(input("Digite sua primeira nota: "))
+nota2 = int(input("Digite sua segunda nota: "))
+print(f"A soma da primeira nota {nota1} com a segunda nota {nota2} é: {nota1 + nota2}.\nA média das duas notas é: {(nota1 + nota2)/2}")
+
+#pode fazer criando variáveis também.
