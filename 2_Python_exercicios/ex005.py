f0fd26947b99dcb8a0f2e1c8032d592c8a5854e8
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-05 11:54:23 +0100

    Create ex005.py

---
+++ 2_Python_exercicios/ex005.py
@@ -0,0 +1,5 @@
+#Exercício Python 5: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n = int(input("Digite um número inteiro: "))
+print(f"O sucessor de {n} é {n+1}")
+print(f"O antecessor de {n} é {n-1}")
