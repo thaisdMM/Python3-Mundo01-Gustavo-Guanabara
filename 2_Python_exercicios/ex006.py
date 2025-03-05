53d7a032b97264466f2fff67d9719c04c2318d5d
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-05 13:36:47 +0100

    Update ex006.py

--- 2_Python_exercicios/ex006.py
+++ 2_Python_exercicios/ex006.py
@@ -1,4 +1,4 @@
 #crie algoritimo que leia um numero e mostre o seu dobro, triplo e raiz quadrada
 
 numero = int(input("Digite um número: "))
-print(f"O número digitado é: {numero}. Seu dobro é {numero * 2}, seu triplo é {numero * 3} e sua raiz quadrada é {numero ** (1/2)}")
+print(f"O número digitado é: {numero}. \nSeu dobro é {numero * 2}, \nseu triplo é {numero * 3} \ne sua raiz quadrada é {numero ** (1/2):.3f}")
