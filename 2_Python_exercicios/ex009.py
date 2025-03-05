dea6f96361fbcc1c085183adb45bc4102c5096e0
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-05 13:56:46 +0100

    Create ex009.py

---
+++ 2_Python_exercicios/ex009.py
@@ -0,0 +1,18 @@
+# ler um numero inteiro e imprimir tabuada
+
+# numero = int(input("Digite um número inteiro: "))
+# tabuada = numero * 1, numero * 2, numero * 3, numero * 4, numero * 5, numero * 6, numero * 7, numero * 8, numero * 9, numero * 10
+# print(f"A tabuada de multiplicação do numero {numero} é: \n {tabuada}")
+
+numero = int(input("Digite um número inteiro: "))
+tabuada1 = numero * 1
+tabuada2 = numero * 2
+tabuada3 = numero * 3
+tabuada4 = numero * 4
+tabuada5 = numero * 5
+tabuada6 = numero * 6
+tabuada7 = numero * 7
+tabuada8 = numero * 8
+tabuada9 = numero * 9
+tabuada10 = numero * 10
+print(f"A tabuada de multiplicação do numero {numero} é: \n {numero} x 1 = {tabuada1} \n {numero} x 2 = {tabuada2} \n {numero} x 3 = {tabuada3} \n {numero} x 4 = {tabuada4} \n {numero} x 5 = {tabuada5} \n {numero} x 6 = {tabuada6} \n {numero} x 7 = {tabuada7}  \n {numero} x 8 = {tabuada8} \n {numero} x 9 = {tabuada9} \n {numero} x 10 = {tabuada10}")
