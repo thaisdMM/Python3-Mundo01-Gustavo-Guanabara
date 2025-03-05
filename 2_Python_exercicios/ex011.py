7806e606c28abe144898568b07d85b0deeb91ec5
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-05 14:23:43 +0100

    Update ex011.py
    -area parede e tinta gasta para pintar

---
+++ 2_Python_exercicios/ex011.py
@@ -0,0 +1,8 @@
+#ler largura e altura parede em metros e cacular sua area. Depois caulcualr quantidade de tinta 1 litro pinta area de 2m2
+
+largura_parede = float(input("Digite quantos metros tem a largura da parede: "))
+altura_parede = float(input("Digite quantos metros tem a altura da parede: "))
+area_parede = largura_parede * altura_parede
+tinta_parede = area_parede / 2
+
+print(f"A largura da parede é: {largura_parede} metros e a altura da parede é: {altura_parede} metros.\nDessa forma a área da parede é: {area_parede:.2f} metros quadrados.\nConsiderando que 1 litro de tinta pinta 2 metros quadrados, para printar essa parede precisariámos de {tinta_parede:.2f} litros de tinta")
