e9dfeda4716f7933254c50dac7ad0366cb1cd8d4
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-08 18:52:47 +0100

    Update ex21b_pygame.py

--- 2_Python_exercicios/ex21b_pygame.py
+++ 2_Python_exercicios/ex21b_pygame.py
@@ -1,5 +1,11 @@
 import pygame
 pygame.init() #inicar o pygame
-pygame.mixer.music.load("ex021.mp3")
+pygame.mixer.music.load('/Users/thaismoreira/GITHUB/Python3-Mundo01-Gustavo-Guanabara/2_Python_exercicios/ex021.mp3')
 pygame.mixer.music.play()
-pygame.event.wait() #esperar terminar para encerrar
+
+#pygame.event.wait() 
+#> NAO ESTÁ FUNCIONANDO. AI TEM A SOLUÇAO ABAIXO, MAS ENVOLVE CODIGO QUE NAO FOI ESTUDADO AINDA
+
+# Enquanto o áudio estiver tocando, espera
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)  # Aguarda um pouco para não sobrecarregar o processador
