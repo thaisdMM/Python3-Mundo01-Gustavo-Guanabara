8661805323f1534b4f65af0d63bda8e0a29ae2a6
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-08 17:46:51 +0100

    Update ex016.py

--- 2_Python_exercicios/ex016.py
+++ 2_Python_exercicios/ex016.py
@@ -3,5 +3,6 @@ from math import ceil, floor, trunc
 
 numero = float(input("Digite um número real: "))
 numero_parte_inteira = print(f"Pegando a porção inteira do {numero} com trunc = {trunc(numero)}")
+numero_parte_interia2= print(f"Pegando a porção inteira do {numero} com conversao int(num) = {int(numero)}")
 numero_arredondado_cima = print(f"\nArredondando o {numero} para cima com ceil = {ceil(numero)}")
 numero_arredondado_baixo = print(f"Arredondando o {numero} para baixo com floor = {floor(numero)}")
