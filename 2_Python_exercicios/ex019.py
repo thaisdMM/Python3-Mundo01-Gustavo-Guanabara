d5e7b0d74faff83dfc11654339f904674f6debca
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-08 11:52:26 +0100

    random.choices
    -escolhendo aluno da lista de alunos aleatoriamente

---
+++ 2_Python_exercicios/ex019.py
@@ -0,0 +1,5 @@
+import random
+
+alunos = ["Maria", "José", "Madalena", "Pedro"]
+print(f"Os possíveis alunos para apagar o quadro são {alunos}")
+print(f"O aluno escolhido é: {random.choice(alunos)}")
