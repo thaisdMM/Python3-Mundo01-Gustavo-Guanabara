88930da5733ad59e8910478bb0edeb83e02b7bd5
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-10 09:35:59 +0100

    update ex24 and created ex025

--- 2_Python_exercicios/ex024.py
+++ 2_Python_exercicios/ex024.py
@@ -1,9 +1,9 @@
 nome_cidade = input("Digite o nome da cidade: ")
 primeiro_nome = nome_cidade.split()
 comeca_santo = primeiro_nome[0]
-print(f"O nome começa com SANTO?:")
+print(f"O nome {nome_cidade} começa com SANTO/santo?:")
 print("SANTO" in comeca_santo.upper())
-print(f"\nO nome começa com SANTA?")
+print(f"\nO nome {nome_cidade} começa com SANTA/santa?")
 print("SANTA" in comeca_santo.upper())
-print(f"\nO nome começa com SÃO: ")
+print(f"\nO nome {nome_cidade} começa com SÃO/são?")
 print("SÃO" in comeca_santo.upper())
---
+++ 2_Python_exercicios/ex025.py
@@ -0,0 +1,4 @@
+nome = input("Digite seu nome completo: ")
+tem_silva = "SILVA" in nome.upper()
+print(f"O nome {nome} tem SILVA? ")
+print(tem_silva)
