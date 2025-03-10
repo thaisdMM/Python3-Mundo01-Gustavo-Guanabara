d67032657577229fe561d0b54be3b3de1627ec0a
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-10 14:50:45 +0100

    Update ex022.py

--- 2_Python_exercicios/ex022.py
+++ 2_Python_exercicios/ex022.py
@@ -1,8 +1,23 @@
-nome = input("Digite seu nome completo: ")
+# Exercício Python 22: Crie um programa que leia o nome completo de uma pessoa e mostre:
+# – O nome com todas as letras maiúsculas e minúsculas.
+# – Quantas letras ao todo (sem considerar espaços). 
+# – Quantas letras tem o primeiro nome.
+
+
+nome = input("Digite seu nome completo: ").strip() #para retirar os espaços no inicio e no final
 separa= nome.split()
 nome_sem_espaços = nome.replace(" ", "") # para retirar os espaços entre as letras
 print(f"Maiúsculo: {nome.upper()}")
 print(f"Minúsculo: {nome.lower()}")
 print(f"O tamanho do nome com espaços é: {len(nome)}")
 print(f"O tamanho do nome sem espaços é: {len(nome_sem_espaços)}")
-print(f"O primeiro nome tem {len(separa[0])} letras")
+print(f"O primeiro nome é {separa[0]} e tem {len(separa[0])} letras")
+
+# outra forma
+
+nome2 = input("Digite seu nome completa: ").strip() 
+print("Analisando seu nome...")
+print(f"Maiúsculo: {nome2.upper()}")
+print(f"Minúsculo: {nome2.lower()}")
+print(f"Tamanho do nome sem espaços: {len(nome2) - nome2.count(' ')}")
+print(f"Primeiro nome: {nome2.find(' ')}")
