9174477005ebb5a0d5d67f2d8d3cb1927be165ae
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-10 15:23:51 +0100

    Update ex024.py
    - resolução professor

--- 2_Python_exercicios/ex024.py
+++ 2_Python_exercicios/ex024.py
@@ -1,9 +1,16 @@
+# Exercício Python 24: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
+
 nome_cidade = input("Digite o nome da cidade: ")
-primeiro_nome = nome_cidade.split()
-comeca_santo = primeiro_nome[0]
+separa_nome = nome_cidade.split()
+comeca_santo = separa_nome[0]
 print(f"O nome {nome_cidade} começa com SANTO/santo?:")
 print("SANTO" in comeca_santo.upper())
 print(f"\nO nome {nome_cidade} começa com SANTA/santa?")
 print("SANTA" in comeca_santo.upper())
 print(f"\nO nome {nome_cidade} começa com SÃO/são?")
 print("SÃO" in comeca_santo.upper())
+
+# Resolução professor
+
+cid = input("Digite a cidade em que você nasceu: ").strip()
+print(cid[:5].upper() == "SANTO")
