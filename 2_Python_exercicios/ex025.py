30a04597c4f10b03bf4417fd87960bf264c6ea65
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-10 15:29:28 +0100

    strip

--- 2_Python_exercicios/ex025.py
+++ 2_Python_exercicios/ex025.py
@@ -1,4 +1,4 @@
-nome = input("Digite seu nome completo: ")
+nome = input("Digite seu nome completo: ").strip()
 tem_silva = "SILVA" in nome.upper()
 print(f"O nome {nome} tem SILVA? ")
 print(tem_silva)
