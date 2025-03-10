84519aa8274419291d113b48d2499cd598e152d7
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-10 15:37:54 +0100

    Update ex026.py
    - strip e contou mais uma para ficar mais apresentavel pq começa do 0

--- 2_Python_exercicios/ex026.py
+++ 2_Python_exercicios/ex026.py
@@ -1,6 +1,6 @@
-frase = input("Digite uma frase: ")
+frase = input("Digite uma frase: ").strip()
 print(f"Na frase {frase} a letra 'a' minúscula aparece: {frase.count('a')}")
 print(f"Na frase {frase} a letra 'A' maiúscula aparece: {frase.count('A')}")
 print(f"Na frase {frase} o total de letra a é: {frase.upper().count('A')}")
-print(f"Na frase {frase} a letra 'a' aparece pela primeira vez: {frase.lower().find('a')}")
-print(f"Na frase {frase} a letra 'a' aparece pela última vez: {frase.lower().rfind('a')}")# preciso conferir essa parte
+print(f"Na frase {frase} a letra 'a' aparece pela primeira vez: {frase.lower().find('a') +1}") # pq começa do 0
+print(f"Na frase {frase} a letra 'a' aparece pela última vez: {frase.lower().rfind('a') +1}")# preciso conferir essa parte
