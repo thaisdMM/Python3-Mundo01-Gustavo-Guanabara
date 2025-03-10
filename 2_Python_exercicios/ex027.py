88763381e13e24148530970993f0343dba02a994
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-10 15:51:13 +0100

    Update ex027.py

--- 2_Python_exercicios/ex027.py
+++ 2_Python_exercicios/ex027.py
@@ -1,5 +1,6 @@
-nome = input("Digite o seu nome completo: ")
+nome = input("Digite o seu nome completo: ").strip()
 nome_separado = nome.split()
 print(f"O nome digitato foi {nome}")
 print(f"Primeiro nome: {nome_separado[0]}")
 print(f"Último nome: {nome_separado[-1]}")
+# print(f"Último nome: {nome_separado[len(nome_separado)-1]}") #codigo do professor
