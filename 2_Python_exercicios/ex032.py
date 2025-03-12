4a96de21c9bd7e8db71f5ce8d130f5b64411db85
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-12 16:45:46 +0100

    Update ex032.py
    - ano bissexto correcao prof

--- 2_Python_exercicios/ex032.py
+++ 2_Python_exercicios/ex032.py
@@ -3,14 +3,22 @@
 # ano bissexto acontece de 4 em 4 anos > ano 366 dias > 
 # exceto: exceto anos múltiplos de 100 que não são múltiplos de 400
 
-ano = int(input("Digite um ano para verificar se é bissexto: "))
-verifica_bissexto = ano % 4 == 0 and ano % 100 != 0
-verifica_bissexto_excecao = ano % 400 == 0
+# #MEU CÓDIGO
+# ano = int(input("Digite um ano para verificar se é bissexto: "))
+# verifica_bissexto = ano % 4 == 0 and ano % 100 != 0
+# verifica_bissexto_excecao = ano % 400 == 0
 
-if verifica_bissexto_excecao == True:
-   print(f"O ano {ano} é bissexto.")
+# if verifica_bissexto_excecao == True:
+#    print(f"O ano {ano} é bissexto.")
+# else:
+#    if verifica_bissexto == True:
+#       print(f"O ano {ano} é bissexto!")
+#    else:
+#       print(f"O ano {ano} não é bissexto.")
+
+# CÓDIGO DO PROFESSOR
+ano = int(input("Que ano quer analizar?"))
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+   print(f"O ano {ano} É BISSEXTO.")
 else:
-   if verifica_bissexto == True:
-      print(f"O ano {ano} é bissexto!")
-   else:
-      print(f"O ano {ano} não é bissexto.")
+   print(f"O ano {ano} NÃO É BISSEXTO.")
