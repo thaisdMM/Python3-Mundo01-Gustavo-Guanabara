1fecb8ef7fc5df5088fb6f2c067e500634c46528
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-12 14:09:16 +0100

    if ==

--- 2_Python_exercicios/ex033.py
+++ 2_Python_exercicios/ex033.py
@@ -1,22 +1,25 @@
 # Faça um programa que leia três números a mostra qual é o maior e qual é o menor.
-# eu pus >= e <= para ter sempre uma resposta pq se for igual nao print
 
 n1 = int(input("Digite o primeiro número: "))
 n2 = int(input("Digite o segundo número: "))
 n3 = int(input("Digite o terceiro número: "))
 
 print("\nMaior de 3 números:")
-if n1 >= n2 and n1 >= n3:
-   print(f"O número {n1} é maior ou igual a {n2} e {n3}")
-elif n2 >= n1 and n2 >= n3:
-   print(f"O número {n2} é maior ou igual a {n1} e {n3}")
+if n1 > n2 and n1 > n3:
+   print(f"O número {n1} é maior que {n2} e {n3}")
+elif n2 > n1 and n2 > n3:
+   print(f"O número {n2} é maior que {n1} e {n3}")
+elif n3 > n1 and n3 > n2:
+   print(f"O número {n3} é maior que {n1} e n{n2}")
 else:
-   print(f"O número {n3} é maior ou igual a {n1} e n{n2}")
+   print(f"Alguns dos números digitados são iguais: {n1}, {n2}, {n3}")
 
 print("\nMenor de 3 números:")
-if n1 <= n2 and n1 <= n3:
-   print(f"O número {n1} é menor ou igual a {n1} e {n3}")
-elif n2 <= n1 and n2 <= n3:
-   print(f"O número {n2} é menor ou igual a {n1} e {n3}")
+if n1 < n2 and n1 < n3:
+   print(f"O número {n1} é menor que a {n1} e {n3}")
+elif n2 < n1 and n2 < n3:
+   print(f"O número {n2} é menor que a {n1} e {n3}")
+elif n3 < n1 and n3 < n2:
+   print(f"O número {n3} é menor que a {n1} e {n2}")
 else:
-   print(f"O número {n3} é menor ou igual a {n1} e {n2}")
+   print(f"Algum dos números são iguais: {n1}, {n2}, {n3}")
---
+++ 2_Python_exercicios/ex034.py
@@ -0,0 +1,4 @@
+# Escrava um programa que pargunte o salário de um Funcionário e calcula o valor do seu aumento.
+#Para salários suparioras a R$1.250.00, calcule um aumento de 10%
+#Para os inferioras ou iguais, o aumento é de 15%
+
