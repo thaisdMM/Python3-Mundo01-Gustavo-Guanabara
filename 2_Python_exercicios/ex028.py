ba5dc24ee3a6b6c3bed14b7224cc863dc1a29fe9
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-12 15:39:55 +0100

    update ex028 and ex029

--- 2_Python_exercicios/ex028.py
+++ 2_Python_exercicios/ex028.py
@@ -1,12 +1,17 @@
 # Exercício Python 28: Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
 from random import randint
+from time import sleep # metodo que faz o computador "dormir" por uns segundos
 
 numero_random = randint(0,5)
 #print(numero_random) # pus o print para testar se funcionava corretamente
-print("Tente advinhar o número. Você pode escolher do número 0 a 5.")
+print("-=-" * 21)
+print("Tente advinhar o número... Você pode escolher do número 0 a 5.")
+print("-=-" * 21)
 numero_usuario = int(input("Digite um numero: "))
+print("PROCESSANDO...")
+sleep(2) # para aparecer que está pensando por 2 segundos
 if numero_usuario == numero_random:
    print(f"Você ACERTOU!")
 else:
-   print(f"Você ERROU! O numero era: {numero_random}")
+   print(f"Você ERROU! O numero era: {numero_random} e você digitou {numero_usuario}")
--- 2_Python_exercicios/ex029.py
+++ 2_Python_exercicios/ex029.py
@@ -2,10 +2,12 @@
 # Se velocidade ultrapassar 80Km/h, mostra uma mensagem dizendo que ele foi multado.
 # A multa vai custar R$7.00 por cada Km acima do limite.
 
-velocidade_carro = int(input("Digite a velocidade do carro: "))
+velocidade_carro = float(input("Digite a velocidade do carro: "))
 multa = (velocidade_carro - 80) * 7
 if velocidade_carro > 80:
-   print(f"Sua velocidade é {velocidade_carro}Km/h e você foi multado!")
+   print(f"Sua velocidade é {velocidade_carro}Km/h. O máximo permitito é 80Km/h e você foi multado!")
    print(f"O valor da multa é: R${multa:.2f}")
-else:
-   print(f"Sua velocidade {velocidade_carro}Km/ foi e você está dentro do limite da velocidade.")
+print("Tenha um bom dia, dirija com segurança!") #essa parte do código sempre executa
+# Eu coloquei o else, mas o prof nao colocou, eu sabia que nao precisava, mas quis ser mais completa
+# else:
+#    print(f"Sua velocidade é {velocidade_carro}Km/ e você está dentro do limite da velocidade.")
