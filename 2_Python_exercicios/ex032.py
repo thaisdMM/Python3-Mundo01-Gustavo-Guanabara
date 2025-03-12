f6012f4710f0e808420d42f6eea5134bb99aad8e
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-12 16:48:35 +0100

    Update ex032.py

--- 2_Python_exercicios/ex032.py
+++ 2_Python_exercicios/ex032.py
@@ -17,7 +17,12 @@
 #       print(f"O ano {ano} não é bissexto.")
 
 # CÓDIGO DO PROFESSOR
-ano = int(input("Que ano quer analizar?"))
+from datetime import date
+
+ano = int(input("Que ano quer analizar? Coloque 0 para ano atual: "))
+if ano == 0:
+   ano = date.today().year # pega o ano atual
+
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
    print(f"O ano {ano} É BISSEXTO.")
 else:
