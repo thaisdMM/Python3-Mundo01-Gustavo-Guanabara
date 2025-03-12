c3c61fbc6edd570a71e4605c34c11ddad0ac50d6
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-12 17:14:50 +0100

    Update ex034.py
    - correcao professor

--- 2_Python_exercicios/ex034.py
+++ 2_Python_exercicios/ex034.py
@@ -2,13 +2,21 @@
 #Para salários suparioras a R$1.250.00, calcule um aumento de 10%
 #Para os inferioras ou iguais, o aumento é de 15%
 
-salario = float(input("Qual o seu salário? "))
+salario = float(input("Qual o seu salário? R$"))
 aumento1 = salario + (salario * 10/100)
 #print(aumento1) # testar a variável
 aumento2 = salario + (salario * 15/100)
 #print(aumento2) # testando se variável está funcionando corretamente antes de implentar todo código
 
 if salario > 1250:
-   print(f"Seu salário é R$ {salario} e vai aumentar para {aumento1:.2f}")
+   print(f"Seu salário é R${salario} e vai aumentar para R${aumento1:.2f}")
 else:
-   print(f"Seu salário é R$ {salario} e vai aumentar para {aumento2:.2f}")
+   print(f"Seu salário é R${salario} e vai aumentar para R${aumento2:.2f}")
+
+# Código PROFESSOR
+salario = float(input("\nQual é o salário do funcionário? R$"))
+if salario <= 1250:
+   novo = salario + (salario * 15 / 100)
+else:
+   novo = salario + (salario * 10 / 100)
+print(f"Que ganhava R${salario} vai passar a ganhar agora R${novo}")
