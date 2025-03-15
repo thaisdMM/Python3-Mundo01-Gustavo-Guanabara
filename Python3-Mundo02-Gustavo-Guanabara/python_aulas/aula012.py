a10ecc4d9b56920baf2883445e6aef2f37161827
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-15 14:52:57 +0100

    if, elif, else

---
+++ Python3-Mundo02-Gustavo-Guanabara/python_aulas/aula012.py
@@ -0,0 +1,14 @@
+nome = input("Digite seu nome: ")
+if nome == "Thaís":
+   print("Que nome bonito!")
+
+elif nome == "Pedro" or nome == "Maria" or nome == "Paulo":
+   print("Seu nome é bem popular no Brasil!")
+
+elif nome in "Ana Claudia Jessica Juliana":
+   print("Belo nome feminino.")
+
+else:
+   print("Seu nome é bem normal.")
+
+print(f"Tenha um bom dia, {nome}")
