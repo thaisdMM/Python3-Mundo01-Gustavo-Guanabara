3bc8e889c8d2d303c1677fff74bae4ac68b61d29
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-16 16:27:35 +0100

    Update ex045.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex045.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex045.py
@@ -1,23 +1,27 @@
-from random import randint
+from random import choice
 
 print("-=-" * 13)
 print("Vamos jogar PEDRA, PAPEL OU TESOURA")
 print("-=-" * 13)
 
-numero_computador = randint(1,3)
-print(numero_computador)
-numero_jogador = int(input("\nDigite: 1 para pedra, 2 para pepel, 3 para tesoura: "))
+escolha_computador = ["pedra","papel", "tesoura"]
+print(choice(escolha_computador))
+escolha_jogador = input("\nDigite: pedra ou pepel ou tesoura: ").strip().lower()
+print(escolha_jogador)
+
+
+
 # numero_computador == 1 = "pedra"
 # numero_computador == 2 = "papel"
 # numero_computador == 3 = "tesoura"
 # print(numero_computador)
-if numero_computador == 1 and numero_jogador == 2:
-   print("Computador: pedra, Jogador: papel. Você ganhou")
+# if numero_computador == 1 and numero_jogador == 2:
+#    print("Computador: pedra, Jogador: papel. Você ganhou")
 # if numero_computador == numero_jogador:
 #    print("Empate")
-if numero_computador == 1:
-   jogada_computador = "pedra"
-elif numero_computador == 2:
-   jogada_computador = "papel"
-elif numero_computador == 3:
-   jogada_computador = "tesoura"
+# if numero_computador == 1:
+#    jogada_computador = "pedra"
+# elif numero_computador == 2:
+#    jogada_computador = "papel"
+# elif numero_computador == 3:
+#    jogada_computador = "tesoura"
