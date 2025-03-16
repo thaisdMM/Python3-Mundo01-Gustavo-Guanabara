3cfcbaa6621a950e17ade07adb4da80c46ad7acd
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-16 18:36:24 +0100

    improve code

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex045.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex045.py
@@ -9,20 +9,15 @@ escolha_computador = choice(opcoes_computador)
 #print(escolha_computador)
 
 escolha_jogador = input("\nDigite: pedra ou pepel ou tesoura: ").strip().lower()
-print(escolha_jogador)
+#print(escolha_jogador)
 
-if escolha_computador == "pedra" and escolha_jogador == "tesoura":
-   print(f"\nComputador: {escolha_computador.upper()}, Jogador: {escolha_jogador.upper()}. PEDRA ganha de TESOURA. Computador ganhou.")
+if escolha_computador == "pedra" and escolha_jogador == "tesoura" or escolha_computador == "papel" and escolha_jogador == "pedra" or escolha_computador == "tesoura" and escolha_jogador == "papel":
+   print(f"\nComputador: {escolha_computador.upper()}, Jogador: {escolha_jogador.upper()}. {escolha_computador.upper()} ganha de {escolha_jogador.upper()}. Computador ganhou.")
 
-elif escolha_jogador == "pedra" and escolha_computador == "tesoura":
-   print(f"\nComputador: {escolha_computador.upper()}, Jogador: {escolha_jogador}. PEDRA ganha de TESOURA. Jogador ganhou!")
-elif escolha_computador == "papel" and escolha_jogador == "pedra":
-   print(f"\nComputador: {escolha_computador.upper()}, Jogador: {escolha_jogador.upper()}. PAPEL ganha de PEDRA. Computador ganhou!")
-elif escolha_jogador == "papel" and escolha_computador == "pedra":
-   print(f"\nComputador: {escolha_computador.upper()}, Jogador: {escolha_jogador.upper()}. PAPEL ganha de PEDRA. Jogador ganhou!")
-elif escolha_computador == "tesoura" and escolha_jogador == "papel":
-   print(f"Computador: {escolha_computador.upper()}, Jogador: {escolha_jogador.upper()}. TESOURA ganha de PAPEL. Computador ganhou!")
-elif escolha_jogador == "tesoura" and escolha_computador == "papel":
-   print(f"Computador: {escolha_computador.upper()}, Jogador: {escolha_jogador.upper()}. TESOURA ganha de PAPEL. Jogador ganhou!")
+
+elif escolha_jogador == "pedra" and escolha_computador == "tesoura" or escolha_jogador == "papel" and escolha_computador == "pedra" or escolha_jogador == "tesoura" and escolha_computador == "papel":
+   print(f"\nComputador: {escolha_computador.upper()}, Jogador: {escolha_jogador}. {escolha_jogador.upper()} ganha de {escolha_computador.upper()}. Jogador ganhou!")
+
 else:
    print(f"Computador: {escolha_computador.upper()}, Jogador: {escolha_jogador.upper()}. EMPATE!")
+print("\nJogo finalizado.")
