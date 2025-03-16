b331b9bfd857da11c8d3f9bf9a23549dfb04e45d
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-16 11:10:52 +0100

    number is > or =

---
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex038.py
@@ -0,0 +1,15 @@
+#Escravo um programa que laia dois números intoiros a compara-os. mostrando no tola uma mensagam:
+# - O primeiro valor é maior
+# - O segundo valor é maior
+# - Não chiste valor maior, os dois são iguais
+
+print("\nCOMPARAÇÃO ENTRE DOIS NÚMEROS INTEIROS:")
+numero1 = int(input("\nDigite o primeiro número: "))
+numero2 = int(input("Digite o segundo números: "))
+#print(numero1, numero2)
+if numero1 > numero2:
+   print(f"O primeiro número: {numero1} é maior que o segundo número: {numero2}")
+elif numero2 > numero1:
+   print(f"O segundo número: {numero2} é maior que o primeiro número: {numero1}")
+else:
+   print(f"Os dois números são iguais: {numero1} = {numero2}")
