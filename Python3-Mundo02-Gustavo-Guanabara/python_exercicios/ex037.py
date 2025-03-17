a3f96e86ff0ff8a85ba349cc747557b93f464383
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-17 11:27:39 +0100

    Update ex037.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex037.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex037.py
@@ -5,15 +5,18 @@
 
 print("\nCONVERSAO DE NUMERO PARA BINÁRIO, OCTAL ou HEXADECIMAL")
 numero = int(input("\nDigite um número inteiro: "))
-print(numero)
-escolha = input("""Escolha a forma de conversão:
-                1 binário
-                2 octal
-                3 hexadecimal\n""")
-if escolha == "1":
-   print(f"\nINTEIRO = {numero}, convertido para BINÁRIO = {bin(numero)}")
-elif escolha == "2":
-   print(f"INTEIRO = {numero}, convertido para OCTAL = {oct(numero)}")
+#print(numero)
+print("""Escolha a forma de conversão:
+[ 1 ] binário
+[ 2 ] octal
+[ 3 ] hexadecimal""")
+escolha = int(input("Digite sua opção: "))
+if escolha == 1:
+   print(f"\nINTEIRO = {numero}, convertido para BINÁRIO = {bin(numero)[2:]}")
+elif escolha == 2:
+   print(f"\nINTEIRO = {numero}, convertido para OCTAL = {oct(numero)[2:]}")
 
-elif escolha == "3":
-   print(f"INTEIRO = {numero}, convertido para HEXADECIMAL = {hex(numero)}")
+elif escolha == 3:
+   print(f"\nINTEIRO = {numero}, convertido para HEXADECIMAL = {hex(numero)[2:]}")
+else:
+   print("\nOpção inválida. Tente novamente.")
