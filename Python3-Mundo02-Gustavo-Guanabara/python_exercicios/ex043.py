697f5221253de43d312755a83bdc2954f4a93f6a
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-17 20:07:29 +0100

    Update ex043.py
    - correção do professor e limpando o código

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex043.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex043.py
@@ -1,23 +1,27 @@
-# Desanvolva uma lógica que laia o paso c a altura de uma passoa, calcula sau IMC a mostra sau status, de acordo com a tabala abairo!
-# - Abaixo da 18.5: Abaixo do Poso
-# - Entro 18.5 a 25: Poso ideal
-# - 25 até 30: Sobrapaso
-# - 30 até 40: Obasidada
-# - Acima de 40:Obesidada mórbida
+# Exercício Python 43: Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo:
+# – IMC abaixo de 18,5: Abaixo do Peso
+# – Entre 18,5 e 25: Peso Ideal
+# – 25 até 30: Sobrepeso
+# – 30 até 40: Obesidade
+# – Acima de 40: Obesidade Mórbida
 
 print("\nCALCULO DO IMC:")
-peso = float(input("\nQual o seu peso? "))
-altura = float(input("Qual a sua altura? "))
-imc = peso / (altura * altura)
-print(peso, altura, imc)
+peso = float(input("\nQual o seu peso? (Kg) "))
+altura = float(input("Qual a sua altura? (m) "))
+imc = peso / (altura ** 2)
+# print(peso, altura, imc)
 
 if imc < 18.5:
-   print(f"\nIMC: {imc:.2f} = Abaixo do Peso.")
-elif imc >=18.5 and imc < 25:
-   print(f"\nIMC: {imc:.2f} = Peso Ideal.")
-elif imc >= 25 and imc < 30:
-   print(f"\nIMC: {imc:.2f} = Sobrepeso.")
-elif imc >= 30 and imc < 40:
-   print(f"\nIMC: {imc:.2f} = Obesidade.")
+    print(f"\nIMC: {imc:.1f} = Abaixo do Peso.")
+# CODIGO PROFESSOR:
+# elif imc < 25: # esse foi eu que pensando na correção de outra aula, e funciona perfeitamente
+elif 18.5 <= imc < 25:
+    print(f"\nIMC: {imc:.1f} = Peso Ideal.")
+# elif imc < 30:
+elif 25 <= imc < 30:
+    print(f"\nIMC: {imc:.1f} = Sobrepeso.")
+# elif imc < 40:
+elif 30 <= imc < 40:
+    print(f"\nIMC: {imc:.1f} = Obesidade.")
 else:
-   print(f"\nIMC: {imc:.2f} = Obesidade Mórbida")
+    print(f"\nIMC: {imc:.1f} = Obesidade Mórbida")
