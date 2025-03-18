cc5fcb802ef73370d09624ec5eb8c42146e95efd
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-18 21:31:00 +0100

    correction

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex047.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex047.py
@@ -1,6 +1,11 @@
+# #ESTÁ CORRETO, MAS ABAIXO TEM UMA OPÇAO QUE GASTA MENOS MEMÓRIA, POIS ITERA DE 2 EM 2
+# print("Números PARES entre 1 e 50:")
+# for i in range(1, 51):
+#     if i % 2 == 0:
+#         #print(".") # aqui mosta a iteraçao de 1 em 1, desnecessária
+#         print(i)
+# print("Fim do programa.")
 
-print("Números PARES entre 1 e 50:")
-for i in range(1, 51):
-    if i % 2 == 0:
-        print(i)
+for i in range(2, 51, 2):
+   print(f"{i}", end=", ")
 print("Fim do programa.")
