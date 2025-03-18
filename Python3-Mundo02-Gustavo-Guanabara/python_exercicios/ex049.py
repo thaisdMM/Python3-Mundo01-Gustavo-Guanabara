547d805706fad3a8840cc01761f0f1fda0d51db3
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-18 20:18:00 +0100

    Update ex049.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex049.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex049.py
@@ -1,8 +1,9 @@
 numero = int(input("Digite um número para saber sua tabuada: "))
 
-print("-=-" * 5)
-print(f"Tabuada de {numero}")
+print(f"\nTabuada de {numero}")
 print("-=-" * 5)
 for i in range(1, 11):
     print(f"{numero} x {i:2} = {i * numero}")
+
+print("-=-" * 5)
 print("\nFim do programa.")
