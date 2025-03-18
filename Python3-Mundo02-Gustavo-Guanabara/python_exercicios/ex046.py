d8970ed0b7e095863d7b45484ae0efdda3284182
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-18 19:13:56 +0100

    for and sleep in range

---
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex046.py
@@ -0,0 +1,13 @@
+# Fafa um programa que mostra na tala uma contagem regressiva parg o estouro da fogos de artificio, indo de 10 até O, com uma pausa de 1 segundo antra clas.
+
+from time import sleep
+print("-=-" * 20)
+print("CONTAGEM REGRESSIVA PARA OS FOGOS DE ARTIFÍCIO")
+print("-=-" * 20)
+sleep(1)
+print("\nVai começar:")
+sleep(1)
+for i in range(10, 0, -1):
+   print(i)
+   sleep(1)
+print("🎉💥 FIM 💥🎊")
