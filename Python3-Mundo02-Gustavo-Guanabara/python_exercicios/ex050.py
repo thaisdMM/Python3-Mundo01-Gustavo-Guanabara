34d1a5460d844a18ea915498bb4bc00c4a5f4c91
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-18 20:22:57 +0100

    add str with all numbers %2 ==0

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex050.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex050.py
@@ -1,9 +1,10 @@
 print("Soma dos números pares digitados: \n")
 soma = 0
-numeros_pares = 0
+numeros_pares = " "
 for i in range(0,6):
    numero = int(input("Digite um número inteiro: "))
    if numero % 2 == 0:
-      numeros_pares = numero
+      numeros_pares += f"{numero}, "
       soma += numero
+print(f"Os números pares digitados foram: {numeros_pares}")
 print(f"A soma dos números pares digitados foi: {soma}")
