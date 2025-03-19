567ce1303875709ef7e572f488aa74801b22776c
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-19 10:26:14 +0100

    working

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/3x051.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/3x051.py
@@ -5,6 +5,7 @@ termo1_pa = int(input("Digite o primeiro termos da progressao aritmética: "))
 print(termo1_pa)
 razao_pa = int(input("Digite a razão da progresão aritmética: "))
 print(razao_pa)
-for i in range(termo1_pa, 11, razao_pa):
+for i in range(0,10):
+   i = termo1_pa
    termo1_pa += razao_pa
    print(i, end= " ")
