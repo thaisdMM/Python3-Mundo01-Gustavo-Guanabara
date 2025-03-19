626383e7333d454f759c82198fb525229dbb9667
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-19 19:59:35 +0100

    erro

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex056.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex056.py
@@ -3,6 +3,10 @@
 # › Qual é o nome do homem mais valho.
 # › Quantas mulheras têm manos de 20 anOs.
 soma_idades = 0
+mulher_menor_20 = 0
+maior_idade_homem = 0
+homem_mais_velho_range = False
+nome_homem_mais_velho = " "
 
 for i in range(0,4):
    nome = input("Digite o seu nome: ").strip().upper()
@@ -11,5 +15,19 @@ for i in range(0,4):
    soma_idades += idade
    media_idade = soma_idades // 4
 
+   if sexo == 2 and idade < 20:
+      mulher_menor_20 += 1
+
+   
+   if sexo == 1:
+      homem_mais_velho_range = True
+      maior_idade_homem = idade
+      nome_homem_mais_velho = nome
+   if idade > maior_idade_homem:
+      maior_idade_homem = idade
+      nome_homem_mais_velho = nome
+
 print(media_idade)
-print(nome, idade, sexo)
+print(maior_idade_homem)
+print(nome_homem_mais_velho)
+print(mulher_menor_20)
