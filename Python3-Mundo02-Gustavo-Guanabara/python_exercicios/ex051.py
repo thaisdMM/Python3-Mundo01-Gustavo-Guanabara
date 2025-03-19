62e9db1d37ae0af970538448bff8c5375fdd0bd9
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-19 11:10:49 +0100

    update ex051

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex051.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex051.py
@@ -3,6 +3,7 @@
 print("PROGRESSÃO ARITMÉTICA")
 termo1_pa = int(input("\nDigite o primeiro termos da progressao aritmética: "))
 razao_pa = int(input("Digite a razão da progresão aritmética: "))
+print(f"\nOs 10 primeiros termos da progressao aritmética que começa com {termo1_pa} e tem razão: {razao_pa} são:")
 for i in range(0,10):
    print(f"{termo1_pa}", end = ", ")
    termo1_pa += razao_pa
---
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex052.py
@@ -0,0 +1,3 @@
+#Faça um programa que laia um número inteiro a diga se cla é ou não um número primo.
+print("DESCOBRINDO SE É NÚMERO PRIMO")
+numero = int(input("Digite um número para saber se é ou não número primo: "))
