a49fa8b2b7933e07684e08a2dacc56fe184970a2
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-19 14:40:41 +0100

    palindromo with for

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex053.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex053.py
@@ -6,13 +6,22 @@
 # A TORRE DA DERROTA O LOBO AMA O BOLO
 # ANOTARAM A DATA DA MARATONA
 
-print("VERIFICANDO SE É PALINDROMO")
-frase = input("Digite uma frase para verificarmos se ela é palindromo: ").strip().lower()
-frase_sem_espaços = frase.replace(" ", "" )
-print(frase)
-print(frase_sem_espaços)
-print(frase_sem_espaços[::-1])
-if frase_sem_espaços == frase_sem_espaços[::-1]:
-   print("PALINDROMO")
-# for i in range(frase_sem_espaços):
-#    if frase_sem_espaços ==
+# # FIZ O PALÍNDROMO, MAS SEM O LAÇO FOR.
+# print("VERIFICANDO SE É PALINDROMO")
+# frase = input("Digite uma frase para verificarmos se ela é palindromo: ").strip().lower()
+# frase_sem_espaços = frase.replace(" ", "" )
+# print(frase)
+# print(frase_sem_espaços)
+# print(frase_sem_espaços[::-1])
+# if frase_sem_espaços == frase_sem_espaços[::-1]:
+#    print("PALINDROMO")
+
+#PALINDROMO COM O LAÇO FOR
+frase = input("Digite uma frase para verificar se a frase é PALÍNDROMO: ").strip().lower()
+frase_sem_espaços = frase.replace(" ", "")
+for i in range(len(frase_sem_espaços)):
+   if frase_sem_espaços == frase_sem_espaços[::-1]:
+      palindromo = "É PALÍNDROMO"
+   else:
+      palindromo = "NÃO É PALÍNDROMO"
+print(palindromo)
