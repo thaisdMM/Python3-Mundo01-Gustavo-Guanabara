668105cbe4393b32cf0395517cea6237931c9f71
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-19 11:38:58 +0100

    Update ex052.py
    começando a criar a lógica

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex052.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex052.py
@@ -1,3 +1,9 @@
 #Faça um programa que laia um número inteiro a diga se cla é ou não um número primo.
 print("DESCOBRINDO SE É NÚMERO PRIMO")
 numero = int(input("Digite um número para saber se é ou não número primo: "))
+vezes_divisao_numero = 0
+
+for i in range(1, numero +1):
+   if numero % i == 0:
+      vezes_divisao_numero += 1
+      print(vezes_divisao_numero) 
