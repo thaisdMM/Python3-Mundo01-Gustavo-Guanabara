cab00809c5c241ba10ce3950f9fab49f149b71f7
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-19 18:58:59 +0100

    Update ex055.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex055.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex055.py
@@ -1,15 +1,19 @@
 #Faça um programa que laig o paso de cinco passoas. No final, mostra qual foi o maior a o menor paso lidos.
 
 maior_peso = 0
+menor_peso = 0
 
-for i in range(0):
-   peso = float(input("Digite o seu peso: "))
-   #print(peso)
-   maior_peso = peso
 for i in range (0,4):
    peso = float(input("Digite o seu peso: "))
+   if i == 0:
+      maior_peso = peso
+      menor_peso = peso
    if peso > maior_peso:
       maior_peso = peso
+   if peso < menor_peso:
+      menor_peso = peso
    else:
       maior_peso = maior_peso
+      menor_peso = menor_peso
 print(maior_peso)
+print(menor_peso)
