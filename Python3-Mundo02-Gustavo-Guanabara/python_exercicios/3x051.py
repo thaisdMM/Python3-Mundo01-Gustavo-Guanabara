c1ae445eb62b47a8e76355465af34dabd859456e
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-19 10:28:54 +0100

    clean code

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/3x051.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/3x051.py
@@ -1,11 +1,9 @@
 #Dasenvolva um programa que laia o primairo tormo e a razão de uma PA. No Final, mostra os 10 princiros termos dessa progressão.
 
 print("PROGRESSÃO ARITMÉTICA")
-termo1_pa = int(input("Digite o primeiro termos da progressao aritmética: "))
-print(termo1_pa)
+termo1_pa = int(input("\nDigite o primeiro termos da progressao aritmética: "))
 razao_pa = int(input("Digite a razão da progresão aritmética: "))
-print(razao_pa)
 for i in range(0,10):
-   i = termo1_pa
+   print(f"{termo1_pa}", end = ", ")
    termo1_pa += razao_pa
-   print(i, end= " ")
+print("\nFim do programa.")
