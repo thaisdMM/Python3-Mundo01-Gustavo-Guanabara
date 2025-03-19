f3cc245cd11fe4ce96ca29f020d51bc1e53455d2
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-19 15:58:02 +0100

    Update ex054.py
    -com string para salvar as idades

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex054.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex054.py
@@ -3,16 +3,19 @@
 from datetime import date
 
 ano_atual = date.today().year
-print(ano_atual)
+#print(ano_atual)
 #ano_nascimento = int(input("Qual ano você nasceu? "))
 #idade = ano_atual - ano_nascimento
 #print(idade)
 maioridade = 0
+salvar_idade = " "
 
 for i in range(0,7):
    ano_nascimento = int(input("Qual ano você nasceu? "))
    idade = ano_atual - ano_nascimento
-   print(idade)
+#   print(idade)
+   salvar_idade += f"{idade}, "
    if idade >= 21:
       maioridade += 1
+print(f"A idade de todos são: {salvar_idade}")
 print(f"Considerando 21 anos maioridade. Das 7 pessoas, {maioridade} são maiores de idade.")
