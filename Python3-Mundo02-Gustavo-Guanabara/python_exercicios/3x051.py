0c014948e959de1392450bc7e0581e9f4eed6506
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-19 10:13:07 +0100

    Create 3x051.py
    - erro, nao conta 10x

---
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/3x051.py
@@ -0,0 +1,10 @@
+#Dasenvolva um programa que laia o primairo tormo e a razão de uma PA. No Final, mostra os 10 princiros termos dessa progressão.
+
+print("PROGRESSÃO ARITMÉTICA")
+termo1_pa = int(input("Digite o primeiro termos da progressao aritmética: "))
+print(termo1_pa)
+razao_pa = int(input("Digite a razão da progresão aritmética: "))
+print(razao_pa)
+for i in range(termo1_pa, 11, razao_pa):
+   termo1_pa += razao_pa
+   print(i, end= " ")
