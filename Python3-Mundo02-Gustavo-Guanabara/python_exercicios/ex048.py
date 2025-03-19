eb65cc26a41e8d9be837280d6ef964858e0a63d9
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-19 18:01:19 +0100

    Update ex048.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex048.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex048.py
@@ -1,8 +1,20 @@
-print("NÚMEROS IMPARES MULTIPLOS DE 3 DE 1 A 500: \n")
+# MEU CÓDIGO
+# print("NÚMEROS IMPARES MULTIPLOS DE 3 DE 1 A 500: \n")
 
-soma = 0
-for i in range(1,501):
-   if i % 2 == 1 and i % 3 == 0:
-      print(i)
+# soma = 0
+# for i in range(1,501):
+#    if i % 2 == 1 and i % 3 == 0:
+#       print(i)
+#       soma += i
+# print(f"\nA soma dos números ímpares multiplos de 3 de 1 a 500 é = {soma}")
+
+# CÓDIGO PROFESSOR tem acumulador e contador
+soma = 0 
+contador_numeros = 0
+# fazendo com o pulo de 2 em 2 e começando com 1 já seleciona só ímpar
+for i in range(1, 501, 2):
+   if i % 3 == 0:
+      contador_numeros +=1
       soma += i
-print(f"\nA soma dos números ímpares multiplos de 3 de 1 a 500 é = {soma}")
+#      print(i, end=" ")
+print(f"Temos {contador_numeros} números ímpares multiplos de 3 do 1 a 500.\nA soma de todos os valores é {soma}")
