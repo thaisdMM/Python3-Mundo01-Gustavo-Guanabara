62688631dae8f7063de16ceadfb3a7851a49197b
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-20 18:36:39 +0100

    Update ex055.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex055.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex055.py
@@ -1,4 +1,6 @@
-# Faça um programa que laig o paso de cinco passoas. No final, mostra qual foi o maior a o menor paso lidos.
+# Exercício Python 55: Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+
+# MEU CÓDIGO É PARECIDO COM O DO PROFESSOR, Mas nao tem o else
 
 for i in range(0, 5):
     peso = float(input("Digite o seu peso: "))
@@ -15,5 +17,25 @@ for i in range(0, 5):
     #    maior_peso = maior_peso
     #    menor_peso = menor_peso
 
-print(f"Maior peso: {maior_peso}")
-print(f"Menor peso: {menor_peso}")
+print(f"Maior peso: {maior_peso}kg")
+print(f"Menor peso: {menor_peso}kg")
+
+# CORREÇÃO PROFESSOR
+
+maior_peso = 0
+menor_peso = 0
+
+for i in range(1, 6):
+    peso = float(input(f"Peso da {i}ª pessoa: "))
+    if i == 1:
+        maior_peso = peso
+        menor_peso = peso
+    else:
+        if peso > maior_peso:
+            maior_peso = peso
+
+        if peso < menor_peso:
+            menor_peso = peso
+
+print(f"\nO MAIOR PESO É: {maior_peso}Kg.")
+print(f"O MENOR PESO É: {menor_peso}Kg.")
