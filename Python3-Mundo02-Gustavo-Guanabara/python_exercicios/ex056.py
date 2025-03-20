4a9de5c024ce6d4cbad973fc8015668055ab8a72
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-20 11:09:58 +0100

    program working

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex056.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex056.py
@@ -1,4 +1,4 @@
-#Desanvolva um programa que laig o nome, idade o sexo da 4 passoas. No final do programa, mostra: 
+# Desanvolva um programa que laig o nome, idade o sexo da 4 passoas. No final do programa, mostra:
 # A média da idade do grupo.
 # › Qual é o nome do homem mais valho.
 # › Quantas mulheras têm manos de 20 anOs.
@@ -8,26 +8,31 @@ maior_idade_homem = 0
 homem_mais_velho_range = False
 nome_homem_mais_velho = " "
 
-for i in range(0,4):
-   nome = input("Digite o seu nome: ").strip().upper()
-   idade = int(input("Qual a sua idade? "))
-   sexo = int(input("SEXO: 1 para MASCULINO ou 2 para FEMININO: "))
-   soma_idades += idade
-   media_idade = soma_idades // 4
+for i in range(0, 4):
+    nome = input("Digite o seu nome: ").strip().upper()
+    idade = int(input("Qual a sua idade? "))
+    print("Informe qual é o seu SEXO?")
+    sexo = int(input("""
+   [ 1 ] MASCULINO 
+   [ 2 ] FEMININO \n"""))
+    soma_idades += idade
 
-   if sexo == 2 and idade < 20:
-      mulher_menor_20 += 1
+    if sexo == 2 and idade < 20:
+        mulher_menor_20 += 1
 
-   
-   if sexo == 1:
-      homem_mais_velho_range = True
-      maior_idade_homem = idade
-      nome_homem_mais_velho = nome
-   if idade > maior_idade_homem:
-      maior_idade_homem = idade
-      nome_homem_mais_velho = nome
+    if sexo == 1:
+        if homem_mais_velho_range == False:
+            homem_mais_velho_range = True
+            maior_idade_homem = idade
+            nome_homem_mais_velho = nome
+        else:
+            if maior_idade_homem < idade:
+                maior_idade_homem = idade
+                nome_homem_mais_velho = nome
 
-print(media_idade)
-print(maior_idade_homem)
-print(nome_homem_mais_velho)
-print(mulher_menor_20)
+media_idade = soma_idades // 4
+print(f"\nMÉDIA DAS IDADES = {media_idade}")
+print(f"IDADE DO HOMEM MAIS VELHO = {maior_idade_homem}")
+print(f"NOME DO HOMEM MAIS VELHO: {nome_homem_mais_velho}")
+print(f"NÚMERO DE MULHERES COM MENOS DE 20 ANOS = {mulher_menor_20}")
+print("\nPrograma finalizado.")
