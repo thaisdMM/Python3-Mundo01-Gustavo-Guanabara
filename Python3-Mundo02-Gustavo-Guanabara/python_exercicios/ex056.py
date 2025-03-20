5aeeab86727bede853981f31497d5783924e94ec
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-20 19:57:48 +0100

    - if not men and if not woman

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex056.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex056.py
@@ -4,6 +4,8 @@ mulher_menor_20 = 0
 maior_idade_homem = 0
 homem_mais_velho_range = False
 nome_homem_mais_velho = " "
+tem_mulher = False
+tem_homem = False
 
 for i in range(0, 4):
     nome = input("Digite o seu nome: ").strip().upper()
@@ -14,17 +16,15 @@ for i in range(0, 4):
    [ 2 ] FEMININO \n"""))
     soma_idades += idade
 
-    if not sexo == 2:
-        mulher_menor_20 = "Não existem mulheres no grupo"
-    elif sexo == 2 and idade < 20:
-        mulher_menor_20 += 1
+    if sexo == 2:
+        tem_mulher = True
+        if sexo == 2 and idade < 20:
+            mulher_menor_20 += 1
 
-    if not sexo == 1:
-        maior_idade_homem = "Nao existem homens no grupo"
-        nome_homem_mais_velho = "Nao existem homens no grupo"
-    elif sexo == 1:
+    if sexo == 1:
+        tem_homem = True
       # PRIMEIRO homem encontrado
-        if homem_mais_velho_range == False:  # checar o primeiro e salvar os dados do primeiro
+        if not homem_mais_velho_range:  # checar o primeiro e salvar os dados do primeiro
             homem_mais_velho_range = True
             maior_idade_homem = idade
             nome_homem_mais_velho = nome
@@ -33,11 +33,16 @@ for i in range(0, 4):
             if maior_idade_homem < idade:
                 maior_idade_homem = idade
                 nome_homem_mais_velho = nome
-        
 
 media_idade = soma_idades // 4
 print(f"\nMÉDIA DAS IDADES = {media_idade}")
-print(f"IDADE DO HOMEM MAIS VELHO = {maior_idade_homem}")
-print(f"NOME DO HOMEM MAIS VELHO: {nome_homem_mais_velho}")
-print(f"NÚMERO DE MULHERES COM MENOS DE 20 ANOS = {mulher_menor_20}")
+if tem_homem:
+    print(f"IDADE DO HOMEM MAIS VELHO = {maior_idade_homem}")
+    print(f"NOME DO HOMEM MAIS VELHO: {nome_homem_mais_velho}")
+else:
+    print("Não existem homens no grupo!")
+if tem_mulher:
+    print(f"NÚMERO DE MULHERES COM MENOS DE 20 ANOS = {mulher_menor_20}")
+else:
+    print("Não existem mulheres no grupo!")
 print("\nPrograma finalizado.")
