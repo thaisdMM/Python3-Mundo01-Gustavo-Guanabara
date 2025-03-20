a2190459acdd6feb802913c34034fc367d044f3c
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-20 19:41:24 +0100

    if not men or woman
    - tentando criar excessao para quando nao tem homem ou mulher no grupo

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex056.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex056.py
@@ -1,7 +1,4 @@
-# Desanvolva um programa que laig o nome, idade o sexo da 4 passoas. No final do programa, mostra:
-# A média da idade do grupo.
-# › Qual é o nome do homem mais valho.
-# › Quantas mulheras têm manos de 20 anOs.
+# Exercício Python 56: Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
 soma_idades = 0
 mulher_menor_20 = 0
 maior_idade_homem = 0
@@ -13,14 +10,19 @@ for i in range(0, 4):
     idade = int(input("Qual a sua idade? "))
     print("Informe qual é o seu SEXO?")
     sexo = int(input("""
-   [ 1 ] MASCULINO 
+   [ 1 ] MASCULINO
    [ 2 ] FEMININO \n"""))
     soma_idades += idade
 
-    if sexo == 2 and idade < 20:
+    if not sexo == 2:
+        mulher_menor_20 = "Não existem mulheres no grupo"
+    elif sexo == 2 and idade < 20:
         mulher_menor_20 += 1
 
-    if sexo == 1:
+    if not sexo == 1:
+        maior_idade_homem = "Nao existem homens no grupo"
+        nome_homem_mais_velho = "Nao existem homens no grupo"
+    elif sexo == 1:
       # PRIMEIRO homem encontrado
         if homem_mais_velho_range == False:  # checar o primeiro e salvar os dados do primeiro
             homem_mais_velho_range = True
@@ -31,6 +33,7 @@ for i in range(0, 4):
             if maior_idade_homem < idade:
                 maior_idade_homem = idade
                 nome_homem_mais_velho = nome
+        
 
 media_idade = soma_idades // 4
 print(f"\nMÉDIA DAS IDADES = {media_idade}")
