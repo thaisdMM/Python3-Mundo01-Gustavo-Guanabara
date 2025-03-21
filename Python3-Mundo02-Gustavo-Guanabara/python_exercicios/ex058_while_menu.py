6093f18e55ecfaaeb25857bb9f342dde3f10004d
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-21 18:36:18 +0100

    Update ex058_while_menu.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex058_while_menu.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex058_while_menu.py
@@ -4,6 +4,7 @@ numero2 = int(input("2° número: "))
 sair_do_programa = False
 soma = numero1 + numero2
 multiplicacao = numero1 * numero2
+maior = 0
 
 while sair_do_programa == False:
     print("""
@@ -18,10 +19,18 @@ while sair_do_programa == False:
     escolha_usuario = int(input("Digite a sua escolha: "))
 
     if escolha_usuario == 1:
-        print(f"Soma: {numero1} + {numero2} = {soma}")
+        print(f"SOMA: {numero1} + {numero2} = {soma}")
+    elif escolha_usuario == 2:
+        print(f"MULTIPLICAÇÃO: {numero1} x {numero2} = {multiplicacao}")
+    elif escolha_usuario == 3:
+        if numero1 > numero2:
+            maior = numero1
+        else:
+            maior = numero2
+        print(f"MAIOR: entre {numero1} e {numero2} o maior é: {maior}")
 
     
-    if escolha_usuario == 5:
+    elif escolha_usuario == 5:
         sair_do_programa = True
         print(f"Escolheu {escolha_usuario}: Sair do programa.")
 
