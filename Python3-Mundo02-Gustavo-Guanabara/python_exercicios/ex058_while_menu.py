45362ba1eaa8e9de9de3e062902b160e068a8774
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-21 18:24:52 +0100

    Create ex058_while_menu.py

---
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex058_while_menu.py
@@ -0,0 +1,22 @@
+numero1 = int(input("1° número: "))
+numero2 = int(input("2° número: "))
+# print(numero1, numero2)
+sair_do_programa = False
+
+while sair_do_programa == False:
+    print("""
+    MENU DO PROGRAMA:
+          
+[1] SOMAR
+[2] MULTIPLICAR
+[3] MAIOR
+[4] NOVOS NÚMEROS
+[5] SAIR DO PROGRAMA
+""")
+    escolha_usuario = int(input("Digite a sua escolha: "))
+    
+    if escolha_usuario == 5:
+        sair_do_programa = True
+        print(f"Escolheu {escolha_usuario}: Sair do programa.")
+
+print("Programa finalizado.")
