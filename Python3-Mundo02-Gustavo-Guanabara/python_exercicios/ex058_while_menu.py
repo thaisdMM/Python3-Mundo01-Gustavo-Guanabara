a93ba874c6ebff88aecbf4ede17f5eee0ead7ab3
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-21 19:13:32 +0100

    cleaned code

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex058_while_menu.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex058_while_menu.py
@@ -1,13 +1,11 @@
 numero1 = int(input("1° número: "))
 numero2 = int(input("2° número: "))
-# print(numero1, numero2)
 sair_do_programa = False
-soma = numero1 + numero2
-multiplicacao = numero1 * numero2
 maior = 0
 
 while sair_do_programa == False:
-    print("""
+    print(
+        """
     MENU DO PROGRAMA:
           
 [1] SOMAR
@@ -15,15 +13,18 @@ while sair_do_programa == False:
 [3] MAIOR
 [4] NOVOS NÚMEROS
 [5] SAIR DO PROGRAMA
-""")
+"""
+    )
     escolha_usuario = int(input("Digite a sua escolha: "))
 
-    if escolha_usuario not in (1,2,3,4,5):
+    if escolha_usuario not in (1, 2, 3, 4, 5):
         print("Escolha inválida. Tente novamente!")
 
     elif escolha_usuario == 1:
+        soma = numero1 + numero2
         print(f"SOMA: {numero1} + {numero2} = {soma}")
     elif escolha_usuario == 2:
+        multiplicacao = numero1 * numero2
         print(f"MULTIPLICAÇÃO: {numero1} x {numero2} = {multiplicacao}")
     elif escolha_usuario == 3:
         if numero1 > numero2:
@@ -32,13 +33,9 @@ while sair_do_programa == False:
             maior = numero2
         print(f"MAIOR: entre {numero1} e {numero2} o maior é: {maior}")
     elif escolha_usuario == 4:
-        novo_numero1 = int(input("Novo 1° número: "))
-        numero1 = novo_numero1
-        novo_numero2 = int(input("Novo 2° número: "))
-        numero2 = novo_numero2
-        soma = numero1 + numero2
-        multiplicacao = numero1 * numero2
-    
+        numero1 = int(input("Novo 1° número: "))
+        numero2 = int(input("Novo 2° número: "))
+
     elif escolha_usuario == 5:
         sair_do_programa = True
         print(f"Escolheu {escolha_usuario}: Sair do programa.")
