202fc8eedf78426fa807687aaf2b29abdd799758
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-22 18:58:51 +0100

    Update ex060_while_for_fatorial.py
    - pronto e codigo limpo

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex060_while_for_fatorial.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex060_while_for_fatorial.py
@@ -1,24 +1,28 @@
-numero = int(input("Digite um número pra saber o fatorial: "))
+print("FATORIAL DE UM NÚMERO COM O FOR:")
+numero = int(input("\nDigite um número pra saber o fatorial: "))
+fatorial = 1
+sequencia_fatorial = " "
+
+for i in range(numero, 0, -1):
+    # print(i)
+    fatorial = fatorial * i
+    if i == 1:
+        sequencia_fatorial += f"{i} "
+    else:
+        sequencia_fatorial += f"{i} x "
+#    print(fatorial)
+#    print(sequencia_fatorial)
+
+print(f"FATORIAL de {numero}: {sequencia_fatorial} = {fatorial}")
+
+print("\nFATORIAL DE UM NÚMERO COM WHILE:")
+numero = int(input("\nDigite um número pra saber o fatorial: "))
 fatorial = 1
 sequencia_fatorial = " "
 numero_fatorial = numero
 
-# for i in range(numero, 0, -1):
-#     #print(i)
-#     fatorial = fatorial * i
-#     if i == 1:
-#         sequencia_fatorial += f"{i} "
-#     else:
-#         sequencia_fatorial += f"{i} x "
-# #    acumulador += fatorial * fatorial
-# #    print(fatorial)
-# #    print(sequencia_fatorial)
-# #    print (acumulador)
-
-# print(f"FATORIAL de {numero}: {sequencia_fatorial} = {fatorial}")
-
 while numero > 0:
-    print(numero)
+    # print(numero)
     fatorial = fatorial * numero
     sequencia_fatorial += f"{numero}"
     numero -= 1
@@ -26,28 +30,6 @@ while numero > 0:
         sequencia_fatorial += f" "
     else:
         sequencia_fatorial += f" x "
-
-    print(fatorial)
-
-#    numero * numero -1
+    # print(fatorial)
 
 print(f"Fatorial de {numero_fatorial}: {sequencia_fatorial} = {fatorial}")
-
-
-
-# while numero > 0:
-# #    print(numero)
-#     fatorial = fatorial * numero
-# #    sequencia_fatorial += f"{numero}"
-#     contador_fatorial = numero -1
-#     # if contador_fatorial == 1:
-#     #     sequencia_fatorial += f"{numero}"
-#     # else:
-#     #     sequencia_fatorial += f" {numero} x "
-
-#     #print(fatorial)
-
-# #    numero * numero -1
-#     print(fatorial)
-#     print(contador_fatorial)
-# print(f"Fatorial de {numero}: {sequencia_fatorial} = {fatorial}")
