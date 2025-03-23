f374b503c3cf375d1cc8cbda041dcf72d3bbcaf5
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-23 10:45:01 +0100

    not working

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex062.while_progressao_aritmetica2.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex062.while_progressao_aritmetica2.py
@@ -1,23 +1,26 @@
-#Melhora o DESAFIO 061, parguntando para o usuário so ale quar mostrar mois alguns termos. O programa encerra quando ala dissar que quar mostrar O tormos.
+# Melhora o DESAFIO 061, parguntando para o usuário so ale quar mostrar mois alguns termos. O programa encerra quando ala dissar que quar mostrar O tormos.
 
 primeiro_termo_pa = int(input("1° termo da Progressão Aritmética: "))
 razao_pa = int(input("Razão da Progressão Aritmética: "))
 termos_pa = primeiro_termo_pa
+# numero_termos_pa = int(input("Número de termos da Progressão Aritmética: "))
 numero_termos_pa = 0
 sequencia_pa = " "
-novos_termos_pa = 0
+novos_termos_pa = -1
 continuar_programa = True
 
 # while continuar_programa == True:
 #     if termos_pa <= 10:
-while numero_termos_pa <= 10:
+while continuar_programa == True:
+    if novos_termos_pa == 0:
+        continuar_programa = False
+    elif termos_pa <= 10:
         print(termos_pa, end=", ")
         termos_pa += razao_pa
         numero_termos_pa += 1
+    elif termos_pa == 10:
+        novos_termos_pa = print(int(input("Quantos termos da PA ainda quer mostrar? ")))
 
-    # if termos_pa == 10:
-    #     novos_termos_pa = print(int(input("Quantos termos da PA ainda quer mostrar? ")))
-
-    # elif novos_termos_pa == 0:
-    #     continuar_programa = False
+# elif novos_termos_pa == 0:
+#     continuar_programa = False
 print("\nPrograma finalizado.")
