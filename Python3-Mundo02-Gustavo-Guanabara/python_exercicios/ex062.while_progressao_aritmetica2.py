f04c50073f98a4348685502d5b517bca86f8b239
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-23 11:01:41 +0100

    Update ex062.while_progressao_aritmetica2.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex062.while_progressao_aritmetica2.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex062.while_progressao_aritmetica2.py
@@ -6,20 +6,28 @@ termos_pa = primeiro_termo_pa
 # numero_termos_pa = int(input("Número de termos da Progressão Aritmética: "))
 numero_termos_pa = 0
 sequencia_pa = " "
-novos_termos_pa = -1
+#novos_termos_pa = -1
 continuar_programa = True
 
 # while continuar_programa == True:
 #     if termos_pa <= 10:
 while continuar_programa == True:
+    novos_termos_pa = int(input("Quantos termos da Progressão Aritmética você quer mostrar? "))
     if novos_termos_pa == 0:
         continuar_programa = False
-    elif termos_pa <= 10:
         print(termos_pa, end=", ")
         termos_pa += razao_pa
         numero_termos_pa += 1
-    elif termos_pa == 10:
-        novos_termos_pa = print(int(input("Quantos termos da PA ainda quer mostrar? ")))
+        numero_termos_pa == novos_termos_pa
+    else:
+        print(novos_termos_pa)
+        #print(termos_pa)
+    # elif termos_pa <= 10:
+    #     print(termos_pa, end=", ")
+    #     termos_pa += razao_pa
+    #     numero_termos_pa += 1
+    # elif termos_pa == 10:
+    #     novos_termos_pa = print(int(input("Quantos termos da PA ainda quer mostrar? ")))
 
 # elif novos_termos_pa == 0:
 #     continuar_programa = False
