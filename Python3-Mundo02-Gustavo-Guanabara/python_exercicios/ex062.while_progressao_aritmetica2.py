dbf4887fe0eba1f4726b91df4176f3e09586fa12
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-24 08:37:00 +0100

    Update ex062.while_progressao_aritmetica2.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex062.while_progressao_aritmetica2.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex062.while_progressao_aritmetica2.py
@@ -6,7 +6,7 @@ termos_pa = primeiro_termo_pa
 # numero_termos_pa = int(input("Número de termos da Progressão Aritmética: "))
 numero_termos_pa = 0
 primeira_sequencia_termos = 10
-sequencia_pa = " "
+sequencia_pa = ""
 novos_termos_pa = 0
 continuar_programa = True
 
@@ -15,7 +15,10 @@ continuar_programa = True
 #     if termos_pa <= 10:
 while continuar_programa:
     print(termos_pa, end=", ")
+    sequencia_pa += f"{termos_pa}, "
     termos_pa += razao_pa
+
+    # print(sequencia_pa)
     # numero_termos_pa += 1
     primeira_sequencia_termos -= 1
     if primeira_sequencia_termos == 0:
@@ -25,22 +28,11 @@ while continuar_programa:
 
         if novos_termos_pa == 0:
             continuar_programa = False
+            print(
+                f"\nVocê digitou {novos_termos_pa} e nao deseja mais adicionar números à Progressão Aritmética."
+            )
+
         else:
             primeira_sequencia_termos = novos_termos_pa
-            print(novos_termos_pa)
-            #    print(primeiro_termo_pa)
-
-            print(f"{(termos_pa)}", end=", ")
-            primeira_sequencia_termos -= 1
-
-        # print(termos_pa)
-    # elif termos_pa <= 10:
-    #     print(termos_pa, end=", ")
-    #     termos_pa += razao_pa
-    #     numero_termos_pa += 1
-    # elif termos_pa == 10:
-    #     novos_termos_pa = print(int(input("Quantos termos da PA ainda quer mostrar? ")))
-
-# elif novos_termos_pa == 0:
-#     continuar_programa = False
+print(f"\nA SEQUENCIA FINAL DA PA foi: {sequencia_pa[:-2]}")
 print("\nPrograma finalizado.")
