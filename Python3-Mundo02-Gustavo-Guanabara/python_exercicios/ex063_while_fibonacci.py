caa87febab4204d4c4b46f8bb6b16866486567f0
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-29 13:55:10 +0100

    Update ex063_while_fibonacci.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex063_while_fibonacci.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex063_while_fibonacci.py
@@ -2,20 +2,43 @@
 
 # Ex: 0 - 1 - 1 - 2 - 3 - 5 - 8
 
-numero_termos_fibonacci = int(input("Quantos termos terão a sequencia fibonacci? "))
+# CORREÇÃO PROFESSOR
+
+print("-" * 30)
+print("Sequência de Fibonacci")
+print("-" * 30)
+numero_termos_fibonacci = int(input("Quantos termos você quer mostrar? "))
 primeiro_termo = 0
 segundo_termo = 1
-contador_termos = 0
-novos_termos = 0
+print("~" * 30)
+print(f"{primeiro_termo} → {segundo_termo}", end="")
+terceiro_termo = primeiro_termo + segundo_termo
+print(f"→ {terceiro_termo}", end="")
 
-while numero_termos_fibonacci > 0:
-    print(f"Primeiro termo: {primeiro_termo}")
-    print(f"Segundo termo: {segundo_termo}")
-    novos_termos = primeiro_termo, segundo_termo
-    primeiro_termo = segundo_termo
-    segundo_termo = primeiro_termo + segundo_termo
+# numero_termos_fibonacci = int(input("Quantos termos terão a sequencia fibonacci? "))
+# primeiro_termo = 0
+# segundo_termo = 1
+# contador_termos = 0
+# novos_termos = 0
+# sequencia_fibonacci = primeiro_termo, segundo_termo
+
+# while numero_termos_fibonacci > 0:
+#     print(f"Primeiro termo: {primeiro_termo}")
+#     numero_termos_fibonacci -= 1
+#     print(f"\nSegundo termo: {segundo_termo}")
+#     numero_termos_fibonacci -=1
+# #    print(f"\nSequência fibonacci: {sequencia_fibonacci}", end="")
+#     #novos_termos = primeiro_termo, segundo_termo
     
-    print(f"Novos termos: {novos_termos}")
-    contador_termos += 1
-    numero_termos_fibonacci -= 1
-print(contador_termos)
+#     novo_primeiro_termo = segundo_termo
+
+# #    numero_termos_fibonacci -= 1
+#     novo_segundo_termo = primeiro_termo + segundo_termo
+
+# #    numero_termos_fibonacci -= 1
+#     sequencia_fibonacci = primeiro_termo, segundo_termo
+    
+#     #print(f"Novos termos: {novos_termos}")
+#     contador_termos += 1
+#     numero_termos_fibonacci -= 1
+# print(contador_termos)
