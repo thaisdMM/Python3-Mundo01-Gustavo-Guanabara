a7f9388e837e4e8501e43252e0470340aa4102da
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-29 11:24:32 +0100

    teacher correction

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex060_while_for_fatorial.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex060_while_for_fatorial.py
@@ -1,4 +1,9 @@
-print("FATORIAL DE UM NÚMERO COM O FOR:")
+# Exercício Python 060: Faça um programa que leia um número qualquer e mostre o seu fatorial.
+#  Exemplo: 5! = 5 x 4 x 3 x 2 x 1 = 120
+
+from math import factorial
+
+print("\nFATORIAL DE UM NÚMERO COM O FOR:")
 numero = int(input("\nDigite um número pra saber o fatorial: "))
 fatorial = 1
 sequencia_fatorial = " "
@@ -32,4 +37,40 @@ while numero > 0:
         sequencia_fatorial += f" x "
     # print(fatorial)
 
-print(f"Fatorial de {numero_fatorial}: {sequencia_fatorial} = {fatorial}")
+print(f"Fatorial de {numero_fatorial}! {sequencia_fatorial} = {fatorial}")
+
+# CORREÇÃO PROFESSOR
+
+# com o MÓDULO math e a função factorial
+
+print("\nFATORIAL COM O MODULO MATH - FACTORIAL:")
+numero = int(input("Digite um número para calcular seu fatorial: "))
+fatorial = factorial(numero)
+print(f"O fatorial de {numero}! é {fatorial}")
+
+# WHILE
+print("\nFATORIAL COM WHILE:")
+numero = int(input("Digite um número pra calcular seu fatorial: "))
+contador = numero
+# 1 é neutro na multiplicação, igual 0 é neutro na soma e subtração
+fatorial = 1
+print(f"Calculando {numero}! = ", end="")
+while contador > 0:
+    print(f"{contador}", end="")
+    print(" x " if contador > 1 else " = ", end="")
+    fatorial *= contador
+    contador -= 1
+
+print(f"{fatorial}")
+
+# Desafio professor: LOOPING FOR
+
+print("\nFATORIAL COM FOR:")
+numero = int(input("Digite um número para calcular seu fatorial: "))
+fatorial = 1
+print(f"Fatorial de {numero}! = ", end="")
+for i in range(numero, 0, -1):
+    print(f"{i}", end="")
+    print(f" x " if i > 1 else " = ", end="")
+    fatorial *= i
+print(f"{fatorial}")
