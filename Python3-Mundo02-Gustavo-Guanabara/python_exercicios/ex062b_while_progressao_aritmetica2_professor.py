3a08b22c2ca9528d69146ba877dba4edb8789f24
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-29 12:19:15 +0100

    Update ex062b_while_progressao_aritmetica2_professor.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex062b_while_progressao_aritmetica2_professor.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex062b_while_progressao_aritmetica2_professor.py
@@ -5,12 +5,13 @@ razao_pa = int(input("Razão da PA: "))
 termo_pa = primeiro_termo
 contador_pa = 1
 total_termos_pa = 0
-mais = 10 # como o programa começa com 10 ele tá simulando que primeiro foi digitado 10 em mais
+mais = 10  # como o programa começa com 10 ele tá simulando que primeiro foi digitado 10 em mais
 while mais != 0:
+    total_termos_pa += mais
     while contador_pa <= total_termos_pa:
         print(f"{termo_pa} → ", end="")
         termo_pa += razao_pa
         contador_pa += 1
     print("PAUSA")
     mais = int(input("Quantos termos você quer mostrar a mais? "))
-print("FIM")
+print(f"Progressao Aritimética finalizada com {total_termos_pa} termos no total.")
