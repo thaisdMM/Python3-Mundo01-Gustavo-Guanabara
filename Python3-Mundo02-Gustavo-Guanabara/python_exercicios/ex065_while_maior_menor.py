02bab8a80dcab2a6d78fb58d4b80d817f562eb62
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-29 15:32:11 +0100

    Create ex065_while_maior_menor.py

---
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex065_while_maior_menor.py
@@ -0,0 +1,17 @@
+# Exercício Python 065: Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+
+soma = 0
+contador = 0
+continuar = True
+
+while continuar:
+    numero = int(input("Digite um número: "))
+    contador +=1
+    soma += numero
+    continuar = input("Quer continuar? [N] para  NÃO. ").strip().upper()[0]
+    if continuar in "Nn":
+        print(f"Você digitou {continuar} e deseja sair do programa.")
+        continuar = False
+        
+print(f"Você digitou {contador} números. A soma entre eles foi {soma}")
+print(f"A média dos números digitados foi {soma / contador}")
