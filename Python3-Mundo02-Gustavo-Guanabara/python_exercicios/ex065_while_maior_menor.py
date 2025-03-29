f223984defae130f4a6316027b65d460404ac7e2
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-29 15:55:13 +0100

    Update ex065_while_maior_menor.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex065_while_maior_menor.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex065_while_maior_menor.py
@@ -1,17 +1,30 @@
-# Exercício Python 065: Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+# # Exercício Python 065: Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 
-soma = 0
-contador = 0
-continuar = True
+# soma = 0
+# contador = 0
+# continuar = True
 
-while continuar:
+# while continuar:
+#     numero = int(input("Digite um número: "))
+#     contador += 1
+#     soma += numero
+#     continuar = input("Quer continuar? [N] para  NÃO. ").strip().upper()[0]
+#     if continuar in "Nn":
+#         print(f"Você digitou {continuar} e deseja sair do programa.")
+#         continuar = False
+
+# print(f"Você digitou {contador} números. A soma entre eles foi {soma}")
+# print(f"A média dos números digitados foi {soma / contador}")
+
+# CORREÇÃO PROFESSOR
+
+resposta = "S"
+media = soma = contador = 0
+while resposta in "Ss":
     numero = int(input("Digite um número: "))
-    contador +=1
     soma += numero
-    continuar = input("Quer continuar? [N] para  NÃO. ").strip().upper()[0]
-    if continuar in "Nn":
-        print(f"Você digitou {continuar} e deseja sair do programa.")
-        continuar = False
-        
-print(f"Você digitou {contador} números. A soma entre eles foi {soma}")
-print(f"A média dos números digitados foi {soma / contador}")
+    contador += 1
+
+    resposta = input("Quer continuar? [S/N]").strip().upper()[0]
+media = soma / contador
+print(f"Você digitou {contador} números e média entre eles foi {media}")
