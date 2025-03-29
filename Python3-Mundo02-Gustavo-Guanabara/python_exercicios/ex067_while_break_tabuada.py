006cd5af1b45baf751574eadfdc46f10edaa29d7
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-29 18:52:03 +0100

    - while and for math table

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex067_while_break_tabuada.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex067_while_break_tabuada.py
@@ -1,6 +1,21 @@
 print(f"=-=" * 10, "TABUADA", "=-=" * 10)
 print("\n0 e números negativos param o programa.")
-contador = 0
+# contador = 1
+while True:
+    print("-" * 40)
+    numero = int(input("\nDigite um número para ver sua tabuada: "))
+    print("-" * 40)
+    contador = 1
+    if numero < 0:
+        break
+    while contador <= 10:
+        print(f"{numero} x {contador:2} = {numero * contador}")
+        contador += 1
+
+print(f"Fim do Programa.")
+
+# programa com WHILE AND FOR
+
 while True:
     print("-" * 40)
     numero = int(input("\nDigite um número para ver sua tabuada: "))
@@ -9,4 +24,5 @@ while True:
         break
     for i in range(1, 11):
         print(f"{numero} x {i:2} = {numero * i}")
+
 print(f"Fim do Programa.")
