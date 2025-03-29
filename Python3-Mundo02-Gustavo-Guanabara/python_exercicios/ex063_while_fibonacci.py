29c946da35c9254e8e53dc7473f4906cca924ece
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-03-29 14:01:24 +0100

    Update ex063_while_fibonacci.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex063_while_fibonacci.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex063_while_fibonacci.py
@@ -12,33 +12,9 @@ primeiro_termo = 0
 segundo_termo = 1
 print("~" * 30)
 print(f"{primeiro_termo} → {segundo_termo}", end="")
-terceiro_termo = primeiro_termo + segundo_termo
-print(f"→ {terceiro_termo}", end="")
-
-# numero_termos_fibonacci = int(input("Quantos termos terão a sequencia fibonacci? "))
-# primeiro_termo = 0
-# segundo_termo = 1
-# contador_termos = 0
-# novos_termos = 0
-# sequencia_fibonacci = primeiro_termo, segundo_termo
-
-# while numero_termos_fibonacci > 0:
-#     print(f"Primeiro termo: {primeiro_termo}")
-#     numero_termos_fibonacci -= 1
-#     print(f"\nSegundo termo: {segundo_termo}")
-#     numero_termos_fibonacci -=1
-# #    print(f"\nSequência fibonacci: {sequencia_fibonacci}", end="")
-#     #novos_termos = primeiro_termo, segundo_termo
-    
-#     novo_primeiro_termo = segundo_termo
-
-# #    numero_termos_fibonacci -= 1
-#     novo_segundo_termo = primeiro_termo + segundo_termo
-
-# #    numero_termos_fibonacci -= 1
-#     sequencia_fibonacci = primeiro_termo, segundo_termo
-    
-#     #print(f"Novos termos: {novos_termos}")
-#     contador_termos += 1
-#     numero_termos_fibonacci -= 1
-# print(contador_termos)
+contador_termos = 3 # começa do 3 pois, já tem 1° e 2° termo definidos
+while contador_termos <= numero_termos_fibonacci:
+    terceiro_termo = primeiro_termo + segundo_termo
+    print(f" → {terceiro_termo}", end="")
+    contador_termos += 1
+print(" → FIM")
