f8d5040faab07117a1b467e45d401fd759a52dce
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-04-29 16:12:35 +0200

    Update ex068b_correcao_professor_while_break_jogo_par_impar.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex068b_correcao_professor_while_break_jogo_par_impar.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex068b_correcao_professor_while_break_jogo_par_impar.py
@@ -1,11 +1,32 @@
 from random import randint
 
+vitoria = 0
 while True:
     jogador = int(input("Diga um valor: "))
     computador = randint(0, 10)
     total = jogador + computador
     tipo = " "
     while tipo not in "PI":
-        tipo = input("Par ou Ímpar? ").strip().upper()[0]
+        tipo = input("Par ou Ímpar? [P/I] ").strip().upper()[0]
 
-    print(f"Você jogou {jogador} e o computador {computador}. O total foi {total}")
+    print(
+        f"Você jogou {jogador} e o computador {computador}. O total foi {total}. ",
+        end="",
+    )
+    print("Deu PAR" if total % 2 == 0 else "Deu ÍMPAR")
+    if tipo == "P":
+        if total % 2 == 0:
+            print("Você VENCEU!")
+            vitoria += 1
+        else:
+            print("Você PERDEU!")
+            break
+    if tipo == "I":
+        if total % 2 == 1:
+            print("Você VENCEU!")
+            vitoria += 1
+        else:
+            print("Você PERDEU!")
+            break
+    print("Vamos jogar novamente...")
+print(f"GAME OVER! Você venceu {vitoria} vezes.")
