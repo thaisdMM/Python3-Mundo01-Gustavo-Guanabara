547b1905a6f450876e25b66709bc1065386e4016
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-04-30 11:34:24 +0200

    Update ex071_while_break_simulador_caixa_eletronico.py

--- Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex071_while_break_simulador_caixa_eletronico.py
+++ Python3-Mundo02-Gustavo-Guanabara/python_exercicios/ex071_while_break_simulador_caixa_eletronico.py
@@ -1,7 +1,7 @@
 print("-" * 15)
 print("BANCO MOREIRA")
 print("-" * 15)
-cedulas_50 = cedulas_20 = 0
+cedulas_50 = cedulas_20 = cedulas_10 = 0
 while True:
     valor = int(input("Qual o valor você quer sacar? R$ "))
 
@@ -11,6 +11,10 @@ while True:
             cedulas_50 = valor // 50
         if (valor % 50 != 0) / 20:
             cedulas_20 = (valor % 50) // 20
+        if (valor - ((cedulas_50 * 50) + (cedulas_20 * 20))) / 10:
+            cedulas_10 = (valor - ((cedulas_50 * 50) + (cedulas_20 * 20))) // 10
+        #if valor % 20 != 0: # and (valor % 20) / 10:
+        #    cedulas_10 = (valor % 20) // 10
         #print(valor / 50)
 
     # if valor % 50 == 0:
@@ -29,5 +33,6 @@ while True:
     #     break
 print(f"Total de {cedulas_50} cedulas de R$50")
 print(f"Total de {cedulas_20} cédulas de R$20")
+print(f"Total de {cedulas_10} cédulas de R$10")
 
 print("\nFim do Programa.")
