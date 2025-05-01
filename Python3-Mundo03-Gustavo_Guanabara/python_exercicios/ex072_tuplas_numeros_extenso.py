318826bb48b5ad4007fcd23065b9dfcc8960cf78
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-01 20:11:01 +0200

    Create ex072_tuplas_numeros_extenso.py

---
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex072_tuplas_numeros_extenso.py
@@ -0,0 +1,39 @@
+# Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+numeros_extenso = (
+    "zero",
+    "um",
+    "dois",
+    "três",
+    "quatro",
+    "cinco",
+    "seis",
+    "sete",
+    "oito",
+    "nove",
+    "dez",
+    "onze",
+    "doze",
+    "treze",
+    "quatorze",
+    "quinze",
+    "dezesseis",
+    "dezessete",
+    "dezoito",
+    "dezenove",
+    "vinte",
+)
+while True:
+    numero = int(input("Digite um número de 0 a 20 para ver ele escrito por extenso: "))
+    # print(f"Tupla {numeros_extenso}")
+    # print(numero)
+    # erro
+    # print(numeros_extenso.index[numero])
+    if numero < 0 or numero > 20:
+        print("Número inválido.")
+    else:
+        for contador in range(0, len(numeros_extenso)):
+            # print(contador, end=" ")
+            if numero == contador:
+                print(numeros_extenso[contador])
+        break
