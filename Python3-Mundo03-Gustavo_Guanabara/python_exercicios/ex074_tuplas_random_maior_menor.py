e88d0c14b1955c6daf3c69e9d9762eb3d50a1abe
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-02 11:34:50 +0200

    Create ex074_tuplas_random_maior_menor.py

---
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex074_tuplas_random_maior_menor.py
@@ -0,0 +1,15 @@
+from random import randint
+
+numeros_aletorios_tupla = (randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10))
+
+# numeros_aletorios_tupla = int()
+# for numeros_aletorios in range (1,5):
+#     numeros_aletorios = randint(0,10)
+#     print(numeros_aletorios)
+#     numeros_aletorios_tupla += (numeros_aletorios)
+# print(numeros_aletorios_tupla)
+
+# numeros_tupla = (0,1,2,3,4,5,6,7,8,9,10)
+# print(sample(numeros_tupla), k=5)
+print(numeros_aletorios_tupla)
+print(type(numeros_aletorios_tupla))
