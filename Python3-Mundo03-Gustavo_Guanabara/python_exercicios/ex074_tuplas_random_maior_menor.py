29e15ef4c4a93e5ea8038094847d25c5eeacfce6
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-02 12:33:49 +0200

    Update ex074_tuplas_random_maior_menor.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex074_tuplas_random_maior_menor.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex074_tuplas_random_maior_menor.py
@@ -1,11 +1,17 @@
 from random import randint
+
 maior = menor = contador = 0
-numeros_aletorios_tupla = (randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10))
-print(numeros_aletorios_tupla)
+numeros_aletorios_tupla = (
+    randint(0, 10),
+    randint(0, 10),
+    randint(0, 10),
+    randint(0, 10),
+    randint(0, 10),
+)
+print(f"Os números sorteados entre 0 a 10 foram: {numeros_aletorios_tupla}")
 for maior_menor in numeros_aletorios_tupla:
-    print(f"I: {maior_menor}")
+    # print(f"I: {maior_menor}")
     contador += 1
-    #print(numeros_aletorios_tupla[0])
     if contador == 1:
         maior = maior_menor
         menor = maior_menor
@@ -14,32 +20,8 @@ for maior_menor in numeros_aletorios_tupla:
             maior = maior_menor
         if menor > maior_menor:
             menor = maior_menor
-    print(f"MAIOR: {maior}")
-    print(f"MENOR: {menor}")
-    print(contador)
-
-#     # if numeros_aletorios_tupla[0]:
-#     #      maior = menor = numeros_aletorios_tupla[0]
-#     # else:
-#     #      if maior < numeros_aletorios_tupla:
-#     #           maior = numeros_aletorios_tupla
-#     #      if menor > numeros_aletorios_tupla:
-#     #           menor = numeros_aletorios_tupla
-#     print(f"Maior número: {maior}")
-#     print(f"Menor número: {menor}")
-#     contador += 1
-#     print(contador)
-
-# # numeros_aletorios_tupla = int()
-# # for numeros_aletorios in range (1,5):
-# #     numeros_aletorios = randint(0,10)
-# #     print(numeros_aletorios)
-# #     numeros_aletorios_tupla += (numeros_aletorios)
-# # print(numeros_aletorios_tupla)
-
-# # numeros_tupla = (0,1,2,3,4,5,6,7,8,9,10)
-# # print(sample(numeros_tupla), k=5)
-# print(numeros_aletorios_tupla)
-# print(type(numeros_aletorios_tupla))
-print(f"Maior número: {maior}")
-print(f"Menor número: {menor}")
+    # print(f"MAIOR: {maior}")
+    # print(f"MENOR: {menor}")
+    # print(contador)
+print(f"\nO maior número entre os sorteados foi: {maior}")
+print(f"O menor número entre os sorteados foi: {menor}")
