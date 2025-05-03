d351305f41da74b918e7375c3d9b08b605585126
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-03 13:35:31 +0200

    Update ex076_tuplas_produtos_preco.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex076_tuplas_produtos_preco.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex076_tuplas_produtos_preco.py
@@ -1,22 +1,22 @@
 produtos_precos = (
+    "Livro",
+    34.90,
     "Lápis",
     1.75,
-    "Boracha",
-    2.00,
+    "Canetas",
+    22.30,
     "Caderno",
     15.90,
+    "Mochila",
+    120.32,
+    "Boracha",
+    2.00,
+    "Compasso",
+    9.99,
     "Estojo",
     25.00,
     "Transferidor",
     4.20,
-    "Compasso",
-    9.99,
-    "Mochila",
-    120.32,
-    "Canetas",
-    22.30,
-    "Livro",
-    34.90,
 )
 
 print("-" * 30)
@@ -25,15 +25,15 @@ print("-" * 30)
 estilo = "." * 25
 print(f"Tupla normal: {produtos_precos}")
 
-tupla_strings = tupla_float =()
+tupla_strings = tupla_float = ()
 
-for indice in range (0, len(produtos_precos)):
+for indice in range(0, len(produtos_precos)):
     if indice % 2 == 0:
         print(produtos_precos[indice])
-        tupla_strings += (produtos_precos[indice], )
+        tupla_strings += (produtos_precos[indice],)
     else:
         print(produtos_precos[indice])
-        tupla_float += (produtos_precos[indice], )
+        tupla_float += (produtos_precos[indice],)
 print(f"Tupla de produtos: {tupla_strings}")
 print(f"Tupla de preços: {tupla_float}")
 ordem_preco = sorted(tupla_float)
@@ -46,7 +46,7 @@ print(len(produtos_precos))
 # for produtos in range(len(tupla_strings, len(tupla_float))):
 #    print(f"{produtos} {estilo} R${produtos}")
 # TypeError: len() takes exactly one argument (2 given)
-# 
+#
 #  ERRO SPLIT
 # tupla_separada = (produtos_precos.split())
 # print(tupla_separada)
