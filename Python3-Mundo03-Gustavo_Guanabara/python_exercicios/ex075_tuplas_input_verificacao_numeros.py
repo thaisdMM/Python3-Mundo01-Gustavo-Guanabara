1fc8dba1a6424764b285bc93574e51214c0d9ee4
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-03 10:30:31 +0200

    Update ex075_tuplas_input_verificacao_numeros.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex075_tuplas_input_verificacao_numeros.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex075_tuplas_input_verificacao_numeros.py
@@ -6,7 +6,7 @@ for i in range(1, 5):
     valores += (numero,)
     # print(type(valores))
 print(f"Você digitou os valores: {valores}")
-print(type(valores))
+# print(type(valores))
 print(f"O valor 9 apareceu: {valores.count(9)} vezes.")
 if 3 in valores:
     print(f"O valor 3 apareceu: {valores.index(3)+1}ª posição")
@@ -17,18 +17,3 @@ for verifica_par in valores:
     if verifica_par % 2 == 0:
         par += (verifica_par,)
 print(f"Os numeros pares digitados foram: {par}")
-
-# for numeros in valores:
-#     if numeros
-
-# valores = (int(input("valor: ")))
-# print(type(valores))
-
-# numero = int(input("Digite um valor: "))
-# valor = ()
-# valor = (numero)
-# print(i)
-# print(f"VALOR: {valor}")
-# print(numero)
-# contador += 1
-# valor(numero, numero)
