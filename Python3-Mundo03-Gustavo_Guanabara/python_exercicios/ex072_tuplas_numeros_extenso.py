2fa8c15eb22177752c00530e74cc298edd60b7a4
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-03 15:23:53 +0200

    Update ex072_tuplas_numeros_extenso.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex072_tuplas_numeros_extenso.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex072_tuplas_numeros_extenso.py
@@ -2,46 +2,6 @@
 
 # Meu código
 
-# numeros_extenso = (
-#     "zero",
-#     "um",
-#     "dois",
-#     "três",
-#     "quatro",
-#     "cinco",
-#     "seis",
-#     "sete",
-#     "oito",
-#     "nove",
-#     "dez",
-#     "onze",
-#     "doze",
-#     "treze",
-#     "quatorze",
-#     "quinze",
-#     "dezesseis",
-#     "dezessete",
-#     "dezoito",
-#     "dezenove",
-#     "vinte",
-# )
-# while True:
-#     numero = int(input("Digite um número de 0 a 20 para ver ele escrito por extenso: "))
-#     # print(f"Tupla {numeros_extenso}")
-#     # print(numero)
-#     # erro
-#     # print(numeros_extenso.index[numero])
-#     if numero < 0 or numero > 20:
-#         print("Número inválido.")
-#     else:
-#         for contador in range(0, len(numeros_extenso)):
-#             # print(contador, end=" ")
-#             if numero == contador:
-#                 print(f"Você digitou o número {numeros_extenso[contador]}")
-#         break
-
-# CORREÇÃO PROFESSOR
-
 numeros_extenso = (
     "zero",
     "um",
@@ -65,23 +25,17 @@ numeros_extenso = (
     "dezenove",
     "vinte",
 )
-contador_numeros_extenso = 0
 while True:
-    numero = int(input("Digite um número entre 0 e 20: "))
-    if 0 <= numero <= 20:
-        print(f"Você digitou o número {numeros_extenso[numero]}")
-        contador_numeros_extenso += 1
-        continuar = input("Quer continuar? [S/N] ").strip().upper()[0]
-        #continuar = " "
-        while continuar not in "SN":
-            print("Comando inválido. Tente novamente.")
-            continuar = input("Quer continuar? [S/N] ").strip().upper()[0]
-        
-        if continuar == "N":
-            break
-        if continuar == "S":
-            print("Voce escolheu continuar.")
+    numero = int(input("Digite um número de 0 a 20 para ver ele escrito por extenso: "))
+    # print(f"Tupla {numeros_extenso}")
+    # print(numero)
+    # erro
+    # print(numeros_extenso.index[numero])
+    if numero < 0 or numero > 20:
+        print("Número inválido.")
     else:
-        print("Tente novamente. ", end="")
-print(f"Você exibiu {contador_numeros_extenso} números por extenso.")
-print("Programa finalizado.")
+        for contador in range(0, len(numeros_extenso)):
+            # print(contador, end=" ")
+            if numero == contador:
+                print(f"Você digitou o número {numeros_extenso[contador]}")
+        break
