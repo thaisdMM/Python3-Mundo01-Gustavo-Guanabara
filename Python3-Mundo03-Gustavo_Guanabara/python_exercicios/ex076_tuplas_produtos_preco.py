1de99845fe08c40a33bba7133c7ea64290023efa
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-03 11:58:00 +0200

    Update ex076_tuplas_produtos_preco.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex076_tuplas_produtos_preco.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex076_tuplas_produtos_preco.py
@@ -8,8 +8,7 @@ produtos_precos = (
     "Estojo",
     25.00,
     "Transferidor",
-    4,
-    20,
+    4.20,
     "Compasso",
     9.99,
     "Mochila",
@@ -17,8 +16,7 @@ produtos_precos = (
     "Canetas",
     22.30,
     "Livro",
-    34,
-    90,
+    34.90,
 )
 
 print("-" * 30)
@@ -27,7 +25,29 @@ print("-" * 30)
 estilo = "." * 25
 print(f"Tupla normal: {produtos_precos}")
 
-# ERRO SPLIT
+tupla_strings = tupla_float =()
+
+for indice in range (0, len(produtos_precos)):
+    if indice % 2 == 0:
+        print(produtos_precos[indice])
+        tupla_strings += (produtos_precos[indice], )
+    else:
+        print(produtos_precos[indice])
+        tupla_float += (produtos_precos[indice], )
+print(f"Tupla de produtos: {tupla_strings}")
+print(f"Tupla de preços: {tupla_float}")
+ordem_preco = sorted(tupla_float)
+print(f"Ordem de preço: {ordem_preco}")
+print(len(produtos_precos))
+# for produtos in range(len(tupla_strings, len(tupla_float))):
+#     print(f"{produtos} {estilo} R${produtos}")
+
+# ERRO LEN
+# for produtos in range(len(tupla_strings, len(tupla_float))):
+#    print(f"{produtos} {estilo} R${produtos}")
+# TypeError: len() takes exactly one argument (2 given)
+# 
+#  ERRO SPLIT
 # tupla_separada = (produtos_precos.split())
 # print(tupla_separada)
 # AttributeError: 'tuple' object has no attribute 'split'
