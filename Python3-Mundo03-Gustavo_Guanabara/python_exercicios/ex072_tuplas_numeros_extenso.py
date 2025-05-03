fb0cfd76a74d61af89a1368187e2c8e2e289bc07
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-03 15:23:04 +0200

    Update ex072_tuplas_numeros_extenso.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex072_tuplas_numeros_extenso.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex072_tuplas_numeros_extenso.py
@@ -1,5 +1,7 @@
 # Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 
+# Meu código
+
 # numeros_extenso = (
 #     "zero",
 #     "um",
@@ -63,9 +65,23 @@ numeros_extenso = (
     "dezenove",
     "vinte",
 )
+contador_numeros_extenso = 0
 while True:
     numero = int(input("Digite um número entre 0 e 20: "))
     if 0 <= numero <= 20:
-        break
-    print("Tente novamente. ", end="")
-print(f"Você digitou o número {numeros_extenso[numero]}")
+        print(f"Você digitou o número {numeros_extenso[numero]}")
+        contador_numeros_extenso += 1
+        continuar = input("Quer continuar? [S/N] ").strip().upper()[0]
+        #continuar = " "
+        while continuar not in "SN":
+            print("Comando inválido. Tente novamente.")
+            continuar = input("Quer continuar? [S/N] ").strip().upper()[0]
+        
+        if continuar == "N":
+            break
+        if continuar == "S":
+            print("Voce escolheu continuar.")
+    else:
+        print("Tente novamente. ", end="")
+print(f"Você exibiu {contador_numeros_extenso} números por extenso.")
+print("Programa finalizado.")
