3dca1454339059a88a4ece10cefc8456ca64bd70
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-03 16:08:26 +0200

    Update ex074_tuplas_random_maior_menor.py
    - com correção do professor

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex074_tuplas_random_maior_menor.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex074_tuplas_random_maior_menor.py
@@ -1,5 +1,7 @@
 from random import randint
 
+# MEU CÓDIGO - está correto, mas o do professor é bem mais simples
+
 maior = menor = contador = 0
 numeros_aletorios_tupla = (
     randint(0, 10),
@@ -25,3 +27,18 @@ for maior_menor in numeros_aletorios_tupla:
     # print(contador)
 print(f"\nO maior número entre os sorteados foi: {maior}")
 print(f"O menor número entre os sorteados foi: {menor}")
+
+# CORREÇÃO PROFESSOR
+
+numeros_aleatorios = (
+    randint(1, 10),
+    randint(1, 10),
+    randint(1, 10),
+    randint(1, 10),
+    randint(1, 10),
+)
+print(f"Os valores sorteados foram: ", end="")
+for numeros in numeros_aleatorios:
+    print(f"{numeros}", end=" ")
+print(f"\nO maior valor sorteado foi: {max(numeros_aleatorios)}")
+print(f"O menor valor sorteado foi: {min(numeros_aleatorios)}")
