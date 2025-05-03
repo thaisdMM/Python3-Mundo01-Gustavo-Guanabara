5003b1d949170c909bb09ad26617829c133dfbfd
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-03 16:27:42 +0200

    Update ex075_tuplas_input_verificacao_numeros.py
    - correção professor

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex075_tuplas_input_verificacao_numeros.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex075_tuplas_input_verificacao_numeros.py
@@ -12,8 +12,29 @@ if 3 in valores:
     print(f"O valor 3 apareceu: {valores.index(3)+1}ª posição")
 else:
     print("O valor 3 não foi digitado em nenhuma posição.")
-par = ()
+# par = ()
+print(f"Os valores pares digitados foram: ", end=" ")
 for verifica_par in valores:
     if verifica_par % 2 == 0:
-        par += (verifica_par,)
-print(f"Os numeros pares digitados foram: {par}")
+        print(verifica_par, end=" ")
+        # par += (verifica_par,)
+# print(f"Os numeros pares digitados foram: {par}")
+
+# CORREÇÃO PROFESSOR:
+
+numero = (
+    int(input("\nDigite um número: ")),
+    int(input("Digite outro número: ")),
+    int(input("Digite mais um número: ")),
+    int(input("Digite o último número: ")),
+)
+print(f"Você digitou os valores: {numero}")
+print(f"O valor 9 apareceu {numero.count(9)} vezes.")
+if 3 in numero:
+    print(f"O valor 3 apareceu na {numero.index(3)+1}ª posição.")
+else:
+    print("O valor 3 não foi digitado em nenhuma posição.")
+print(f"Os valores pares digitados foram: ", end=" ")
+for verifica_par in numero:
+    if verifica_par % 2 == 0:
+        print(verifica_par, end=" ")
