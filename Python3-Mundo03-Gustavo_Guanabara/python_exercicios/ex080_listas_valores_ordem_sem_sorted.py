aa9e93e3e3907df3dbc2c655b07c54d20c588dc6
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-04 18:41:45 +0200

    Create ex080_listas_valores_ordem_sem_sorted.py

---
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex080_listas_valores_ordem_sem_sorted.py
@@ -0,0 +1,44 @@
+valores = []
+numero_comparado = posicao = 0
+# Nao dá para ser enumerates
+# for posicao, valor in enumerate(0,5):
+for posicao in range(0, 5):
+    numero = int(input("Digite um valor de 0 a 10: "))
+
+    if posicao == 0:
+        valores += [
+            numero,
+        ]
+        # valores.append(numero)
+        # posicao += 1
+        print(valores)
+        print(posicao)
+    else:
+        for valor in valores:
+            if numero > valor:
+                print(f"O valor {numero} foi adicionado no final da lista...")
+                valores.append(numero)
+            elif numero < valor:
+                valores.insert(0, numero)
+        posicao += 1
+        print(valores)
+        print(posicao)
+
+
+# valores += [numero, ]
+#     if posicao == 0:
+#         if numero > 5:
+#             print(f"O valor {numero} foi adicionado no final da lista...")
+#             valores.insert(5,numero)
+#             print(valores)
+#         elif numero <= 5:
+#             print(f"O valor {numero} foi adicionado na posição 0 da lista...")
+#         numero_comparado = numero
+#    # valores += [numero, ]
+#     if posicao == 1:
+#         if numero < numero_comparado:
+#             posicao
+#         # if numero < numero[0]:
+#             print("Valor menor")
+
+print(f"O valores digitados foram: {valores}")
