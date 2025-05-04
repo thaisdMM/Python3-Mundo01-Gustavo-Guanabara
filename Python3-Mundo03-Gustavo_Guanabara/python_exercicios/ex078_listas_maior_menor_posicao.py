cf0b84fd0a1a7270dfbae19bc4c05c3002d8427c
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-04 14:44:29 +0200

    Update ex078_listas_maior_menor_posicao.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex078_listas_maior_menor_posicao.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex078_listas_maior_menor_posicao.py
@@ -4,37 +4,26 @@ valores = []
 for i in range(0, 5):
     valores.append(int(input(f"Digite um valor para a posição {i}: ")))
 print(f"O valores digitados foram: {valores}")
-print(
-    f"O valor máximo {max(valores)} é aparece na posição: {valores.index(max(valores))}"
-)
-print(
-    f"O valor minimo {min(valores)} é aparece na posição: {valores.index(min(valores))}"
-)
-posicao_maior_valor = []
-posicao_menor_valor = []
+# print(f"O valor máximo {max(valores)} é aparece na posição: {valores.index(max(valores))}")
+# print(f"O valor minimo {min(valores)} é aparece na posição: {valores.index(min(valores))}")
+
+# # usei uma variável para salvar a posição do maior e do menor valor, mas depois acabai exibindo esses valores com o print no for, entao nao foi necessário manter essa variável
+# posicao_maior_valor = []
+# posicao_menor_valor = []
+
+print(f"O valor máximo {max(valores)} aparece na posição: ", end="")
 for posicao, valor in enumerate(valores):
-    print(f"posição {posicao}")
-    print(f"valor: {valor}")
-    #     print(maximo[valores])
+    # print(f"posição {posicao}")
+    # print(f"valor: {valor}")
     if valor == max(valores):
-        posicao_maior_valor += [
-            posicao,
-        ]
-        print(f"Possicao maior valor {posicao_maior_valor}")
+        print(f"{posicao}...", end="")
+        # posicao_maior_valor += [posicao,]
+        # print(f"Possicao maior valor {posicao_maior_valor}")
+print(f"\nO menor valor digitado foi {min(valores)} na posição: ", end="")
+for posicao, valor in enumerate(valores):
     if valor == min(valores):
-        posicao_menor_valor += [
-            posicao,
-        ]
-        print(f"Possicao menor valor {posicao_menor_valor}")
-print(f"O valor máximo {max(valores)} aparece na posição: {posicao_maior_valor}")
-print(f"O menor valor digitado foi {min(valores)} na posição {posicao_menor_valor}")
-# maior = menor = 0
-# for posicao, valor in enumerate(valores):
-#     # if posicao == 0:
-#     #     maior = valor
-#     #     menor = valor
-#     print(f"posição {posicao}", end=" ")
-#     print(f"valor: {valor}")
-# if (max(valores)) in enumerate(valores):
-#     print(f"O maior valor digitado foi {max(valores)} na posiçao {posicao}")
-# print(f"O menor valor digitado foi {min(valores)} na posição {posicao}")
+        print(f"{posicao}...", end="")
+        # posicao_menor_valor += [posicao,]
+        # print(f"Possicao menor valor {posicao_menor_valor}")
+# print(f"O valor máximo {max(valores)} aparece na posição: {posicao_maior_valor}")
+# print(f"O menor valor digitado foi {min(valores)} na posição {posicao_menor_valor}")
