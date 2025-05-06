3a801311cd3cede160a0b07a29b87f27709922fe
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-06 17:46:57 +0200

    Create ex081_listas.py

---
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex081_listas.py
@@ -0,0 +1,25 @@
+valores = []
+contagem = 0
+while True:
+    valor = int(input("Digite um valor: "))
+    valores += [
+        valor,
+    ]
+    continuar = input("Quer continuar? [S/N] ").strip().upper()[0]
+    while continuar not in "SN":
+        print("Resposta inválida. Tente novamente.")
+        continuar = input("Quer continuar? [S/N] ").strip().upper()[0]
+    if continuar == "N":
+        break
+    else:
+        contagem += 1
+print("-=" * 30)
+print(f"Você digitou {len(valores)} valores.")
+valores.sort(reverse=True)
+print(f"Os valores digitados em ordem decrescente são: {valores}")
+if 5 in valores:
+    print(
+        f"O valor 5 faz parte da lista. E se encontra na posição {(valores.index(5))+1}"
+    )
+else:
+    print("O valor 5 não está na lista.")
