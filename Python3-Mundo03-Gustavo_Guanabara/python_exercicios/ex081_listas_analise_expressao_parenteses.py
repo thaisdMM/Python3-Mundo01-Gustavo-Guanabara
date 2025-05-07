1879d4182316ea9c30d3c8cef94b4f088242090d
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-07 20:17:36 +0200

    Update ex081_listas_analise_expressao_parenteses.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex081_listas_analise_expressao_parenteses.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex081_listas_analise_expressao_parenteses.py
@@ -1,48 +1,50 @@
-expressoes_usuarios = input(
+expressoes_usuarios = list(input(
     "Digite uma expressão matemática que use parênteses para ver se a expressão está correta: "
-)
-# expressoes = [expressoes_usuarios]
-# expressoes2 = [(a+b)]
-
-# print(type(expressoes))
-# print(f"Tamaho da lista {len(expressoes)}")
+))
+# print(f"expressoes_usuarios: {expressoes_usuarios}")
+# print(type(expressoes_usuarios))
+# print(f"Tamaho da lista {len(expressoes_usuarios)}")
 
 
 abriu_paretense = fechou_parentese = contador= 0
-for valores in expressoes_usuarios:
-    print(f"Valores: {valores}")
-    if expressoes_usuarios[0] == ")":
-        print("Expressao aberta inválidamente.")
-        break
-    elif expressoes_usuarios[-1] == "(":
-        print("Expressao fechada inválidamente.")
-        break
-    elif valores == ")":
-        fechou_parentese -= 1
-    elif valores == "(":
-        abriu_paretense += 1
-    total_parenteses = fechou_parentese + abriu_paretense
-    if total_parenteses < 0:
-        print("Expressão inválida.")
-        break
+
+if  "(" not in expressoes_usuarios and ")" not in expressoes_usuarios:
+    print(f"A expressão é inválida. Sequer tem parenteses.")
+if expressoes_usuarios[0] == ")":
+    print("Expressao aberta inválidamente.")
+elif expressoes_usuarios[-1] == "(":
+    print("Expressao fechada inválidamente.")
+else:
+    for valores in expressoes_usuarios:
+        # if  "(" not in expressoes_usuarios and ")" not in expressoes_usuarios:
+        #     print(f"A expressão é inválida. Sequer tem parenteses.")
+        #     break
+        # print(f"Valores: {valores}")
+        # if expressoes_usuarios[0] == ")":
+        #     print("Expressao aberta inválidamente.")
+        #     break
+        # elif expressoes_usuarios[-1] == "(":
+        #     print("Expressao fechada inválidamente.")
+        #     break
+        if valores == ")":
+            fechou_parentese -= 1
+        elif valores == "(":
+            abriu_paretense += 1
+        total_parenteses = fechou_parentese + abriu_paretense
+        if total_parenteses < 0:
+            print("Expressão inválida.")
+            break
 
 
-    contador +=1
-    print(contador)
-    print(f"total: {total_parenteses}")
+        contador +=1
+        # print(contador)
+        # print(f"total: {total_parenteses}")
+    if  total_parenteses == 0:
+        print(f"A expressão é valida!")
+    else:
+        print(f"A expressão é inválida.")
+
 
 print(f"A expressão digitada foi: {expressoes_usuarios}")
-print(f"Total de abriu parenteses {abriu_paretense}")
-print(f"Total de fechou parenteses {fechou_parentese}")
-if abriu_paretense == fechou_parentese != 0:
-    print(f"A expressão é valida!")
-else:
-    print(f"A expressão é inválida.")
-
-    # if expressoes_usuarios.find(")") == True:
-    #     print("Expressao aberta inválidamente.")
-    #     break
-
-    # elif expressoes_usuarios[-1] == "(":
-    #     print("Expressao fechada inválidamente.")
-    #     break
+# print(f"Total de abriu parenteses {abriu_paretense}")
+# print(f"Total de fechou parenteses {fechou_parentese}")
