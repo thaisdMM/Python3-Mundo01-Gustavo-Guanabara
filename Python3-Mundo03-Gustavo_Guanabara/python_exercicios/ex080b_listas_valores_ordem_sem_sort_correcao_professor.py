ac0121ac43a141c5273ae21dfaef1ec05b16a57e
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-08 11:18:09 +0200

    Update ex080b_listas_valores_ordem_sem_sort_correcao_professor.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex080b_listas_valores_ordem_sem_sort_correcao_professor.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex080b_listas_valores_ordem_sem_sort_correcao_professor.py
@@ -1,8 +1,6 @@
 valores = []
 for posicao in range (0,5):
     numero = int(input("Digite um valor: "))
-    if posicao == 0:
+    if posicao == 0 or numero > valores[-1]:
         valores.append(numero)
     
-    elif numero > valores[-1]:
-        valores.append(numero)
