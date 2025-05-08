5fc55fa135641638d89579c2191808c327f0d85d
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-08 15:12:46 +0200

    Update ex083_listas_analise_expressao_parenteses.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex083_listas_analise_expressao_parenteses.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex083_listas_analise_expressao_parenteses.py
@@ -1,3 +1,5 @@
+# Exercício Python 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
+
 expressoes_usuarios = list(
     input(
         "Digite uma expressão matemática que use parênteses para ver se a expressão está correta: "
