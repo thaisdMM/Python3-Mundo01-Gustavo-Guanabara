b3b3cad96ce110d760a8660459f451f3a51961c8
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-08 14:57:34 +0200

    Update ex081_listas.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex081_listas.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex081_listas.py
@@ -1,5 +1,9 @@
+# Exercício Python 081: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre:
+#  A) Quantos números foram digitados.
+#  B) A lista de valores, ordenada de forma decrescente.
+# C) Se o valor 5 foi digitado e está ou não na lista.
+
 valores = []
-contagem = 0
 while True:
     valores.append(int(input("Digite um valor: ")))
     continuar = input("Quer continuar? [S/N] ").strip().upper()[0]
@@ -8,8 +12,7 @@ while True:
         continuar = input("Quer continuar? [S/N] ").strip().upper()[0]
     if continuar == "N":
         break
-    # else:
-    #     contagem += 1
+
 print("-=" * 30)
 print(f"Você digitou {len(valores)} valores.")
 valores.sort(reverse=True)
