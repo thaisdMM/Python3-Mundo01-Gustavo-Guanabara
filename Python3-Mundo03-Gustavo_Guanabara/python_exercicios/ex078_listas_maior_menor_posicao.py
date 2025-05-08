d9c7138dae4ebcd9f955bf03e28edcdb3546b8c2
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-08 10:34:48 +0200

    Update ex078 with teacher correction

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex078_listas_maior_menor_posicao.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex078_listas_maior_menor_posicao.py
@@ -16,14 +16,42 @@ for posicao, valor in enumerate(valores):
     # print(f"posição {posicao}")
     # print(f"valor: {valor}")
     if valor == max(valores):
-        print(f"{posicao}...", end="")
+        print(f"{posicao}... ", end="")
         # posicao_maior_valor += [posicao,]
         # print(f"Possicao maior valor {posicao_maior_valor}")
 print(f"\nO menor valor digitado foi {min(valores)} na posição: ", end="")
 for posicao, valor in enumerate(valores):
     if valor == min(valores):
-        print(f"{posicao}...", end="")
+        print(f"{posicao}... ", end="")
         # posicao_menor_valor += [posicao,]
         # print(f"Possicao menor valor {posicao_menor_valor}")
 # print(f"O valor máximo {max(valores)} aparece na posição: {posicao_maior_valor}")
 # print(f"O menor valor digitado foi {min(valores)} na posição {posicao_menor_valor}")
+
+# CORREÇÃO PROFESSOR:
+
+lista_numero = []
+maior = 0
+menor = 0
+for posicao in range(0, 5):
+    lista_numero.append(int(input(f"Digite um valor para a posição {posicao} ")))
+    if posicao == 0:
+        maior = menor = lista_numero[posicao]
+    else:
+        if lista_numero[posicao] > maior:
+            maior = lista_numero[posicao]
+        if lista_numero[posicao] < menor:
+            menor = lista_numero[posicao]
+
+print("=-" * 30)
+print(f"Você digitou os valores {lista_numero}")
+print(f"O maior valor digitado foi {maior} nas posições ", end="")
+for indice, valor in enumerate(lista_numero):
+    if valor == maior:
+        print(f"{indice}... ", end="")
+print()  # para quebrar de linha
+print(f"O menor valor digitado foi {menor} nas posições ", end="")
+for indice, valor in enumerate(lista_numero):
+    if valor == menor:
+        print(f"{indice}...", end="")
+print()
