5301eaf0aba39bd4e73d0ccd38e9c7ef20fcf23c
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-08 11:46:03 +0200

    Update ex080b_listas_valores_ordem_sem_sort_correcao_professor.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex080b_listas_valores_ordem_sem_sort_correcao_professor.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex080b_listas_valores_ordem_sem_sort_correcao_professor.py
@@ -1,6 +1,17 @@
 valores = []
-for posicao in range (0,5):
+for i in range(0, 5):
     numero = int(input("Digite um valor: "))
-    if posicao == 0 or numero > valores[-1]:
+    if i == 0 or numero > valores[-1]:
         valores.append(numero)
-    
+        print("Adicionado ao final da lista...")
+    else:
+        posicao = 0
+        while posicao < len(valores):
+            # print(posicao)
+            if numero <= valores[posicao]:
+                valores.insert(posicao, numero)
+                print(f"Adicionado na posicação {posicao} da lista...")
+                break
+            posicao += 1
+print("=-" * 30)
+print(f"Os valores digitados em ordem foram: {valores}")
