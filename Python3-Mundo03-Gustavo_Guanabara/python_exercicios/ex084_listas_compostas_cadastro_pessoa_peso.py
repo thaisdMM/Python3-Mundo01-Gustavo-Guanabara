6af5c43f70ea474930e41b392d290e10aebca54c
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-09 14:15:51 +0200

    Update ex084_listas_compostas_cadastro_pessoa_peso.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex084_listas_compostas_cadastro_pessoa_peso.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex084_listas_compostas_cadastro_pessoa_peso.py
@@ -10,11 +10,11 @@ lista_menor_peso = list()
 maior_peso = 0
 menor_peso = 0
 while True:
-    dados.append(input("Nome: "))
+    dados.append(input("Nome: ").strip())
     dados.append(float(input("Peso: ")))
     lista_pessoas.append(dados[:])
     dados.clear()
-    print(f"Lista de pessoas: {lista_pessoas}")
+    # print(f"Lista de pessoas: {lista_pessoas}")
     continuar = input("Quer continuar? [S/N] ").strip().upper()[0]
     while continuar not in "SN":
         print("Resposta inválida. Digite [S/N]")
@@ -24,37 +24,36 @@ while True:
 
 print(f"Ao todo você cadastrou {len(lista_pessoas)} pessoas.")
 print(f"A lista de pessoas cadastrada foi: {lista_pessoas}")
-print(f"lista_pessoas[1]: {lista_pessoas[1]}")
-
 
 for posicao, peso in enumerate(lista_pessoas):
-    print(f"Posicao enumarates: {posicao}")
+    # print(f"Posicao enumarates: {posicao}")
+    # print(f"maior_peso: {maior_peso}")
+    # print(f"menor_peso: {menor_peso}")
     if posicao == 0:
-        # print(f"Lista_pessoas[0] {lista_pessoas[0][1]}")
-        print(f"POSICAO[0] {posicao}")
+        # print(f"POSICAO[0] {posicao}")
         if peso[1]:
             maior_peso = peso[1]
             menor_peso = peso[1]
-            lista_maior_peso.append(peso[0])
-            print(f"maior_peso: {maior_peso}")
-            print(f"menor_peso: {menor_peso}")
-            print(f"lista Pessoas_maior_peso: {lista_maior_peso}")
-    # elif lista_pessoas != [0]:
+            # print(f"maior_peso: {maior_peso}")
+            # print(f"menor_peso: {menor_peso}")
     else:
         if peso[1] >= maior_peso:
-            lista_maior_peso.pop()
-            print(f"lista Pessoas_maior_peso: {lista_maior_peso}")
-            print(f"Peso 1 {peso[1]}")
+            # print(f"Peso 1 {peso[1]}")
             maior_peso = peso[1]
-            lista_maior_peso.append(peso[0])
-            print(f"maior_peso: {maior_peso}")
-            print(f"lista Pessoas_maior_peso: {lista_maior_peso}")
-
+            # print(f"maior_peso: {maior_peso}")
         if peso[1] <= menor_peso:
-            print(f"Peso 1 {peso[1]}")
+            # print(f"Peso 1 {peso[1]}")
             menor_peso = peso[1]
-            print(f"menor_peso: {menor_peso}")
+            # print(f"menor_peso: {menor_peso}")
+for pessoa in lista_pessoas:
+    # print(pessoa)
+    if pessoa[1] == maior_peso:
+        lista_maior_peso.append(pessoa[0])
+    if pessoa[1] == menor_peso:
+        lista_menor_peso.append(pessoa[0])
 
 print(f"O maior peso da lista foi {maior_peso} e as pessoas foram: {lista_maior_peso}")
-print(f"\nO menor peso da lista foi {menor_peso} e as pessoas foram: ")
+print(
+    f"\nO menor peso da lista foi {menor_peso} e as pessoas foram: {lista_menor_peso}"
+)
 print("Programa finalizado.")
