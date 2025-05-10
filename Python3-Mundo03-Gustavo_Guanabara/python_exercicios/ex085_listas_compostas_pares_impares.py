f22a294ad4de006a49ddb19b2d687fc2e1f0cd79
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-10 10:50:17 +0200

    Update ex085_listas_compostas_pares_impares.py
    - tentando outras soluçoes conforme o enunciado, mas não está correta

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex085_listas_compostas_pares_impares.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex085_listas_compostas_pares_impares.py
@@ -5,13 +5,18 @@ for valores in range(0,7):
     numero = int(input("Digite um valor: "))
     if numero % 2 == 0:
         pares_lista.append(numero)
+        lista_completa.append(pares_lista[:])
+        pares_lista.clear()
     else:
         impares_lista.append(numero)
-pares_lista.sort()
-impares_lista.sort()
-lista_completa.append(pares_lista)
-lista_completa.append(impares_lista)
+        lista_completa.append(impares_lista[:])
+        impares_lista.clear()
+        
+# pares_lista.sort()
+# impares_lista.sort()
+# lista_completa.append(pares_lista)
+# lista_completa.append(impares_lista)
 
 print(f"A lista de números completos é: {lista_completa}")
-print(f"Os valores pares digitados foram: {pares_lista}")
-print(f"Os valores ímpares digitados forama: {impares_lista}")
+print(f"Os valores pares digitados foram: {lista_completa[0]}")
+print(f"Os valores ímpares digitados forama: {lista_completa[1]}")
