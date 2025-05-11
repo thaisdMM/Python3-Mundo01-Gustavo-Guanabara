6fd7e3bee1ee8f41340df8bc29784980913120d0
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-11 12:06:22 +0200

    Update ex086_listas_compostas_matriz3x3.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex086_listas_compostas_matriz3x3.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex086_listas_compostas_matriz3x3.py
@@ -1,34 +1,19 @@
-# Jeito de fazer, mas não exibia as posicões corretas da lista
-
-matriz_lista = [[], [], []]
-for valor in range(0, 9):
-    if valor < 3:
-        matriz_lista[0].append(int(input(f"Digite um valor para {valor} ")))
-    elif valor < 6:
-        matriz_lista[1].append(int(input(f"Digite um valor para {valor} ")))
-    else:
-        matriz_lista[2].append(int(input(f"Digite um valor para {valor} ")))
-# print(f"Matriz: {matriz_lista}")
-for posicao, valor in enumerate(matriz_lista):
-    print(f"Posicao: {posicao}")
-print("=-" * 30)
-for valor in matriz_lista:
-    print(valor)
-
-# codigo correto: 
-
 matriz_lista = []
 linha_matriz = []
-for contador in range(0,3):
+#for contador in range(0, 3):
+for linha in range(0, 3):
+    # coluna_matriz = print(f"Digite um valor para: [{coluna},", end="")    
     for coluna in range(0, 3):
-        coluna_matriz = print(f"Digite um valor para: [{coluna},", end="")
-        for linha in range(0,1):
-            linha_matriz.append(int(input(f" {linha}] ")))
-            print(f"Linha_matriz: {linha_matriz}")
+        linha_matriz.append(int(input(f"Dogite um valor para [{linha}, {coluna}] ")))
+        #print(f"Digite um valor para: [{coluna},", end="")
+        #linha_matriz.append(int(input(f" {linha}] ")))
+        print(f"Linha_matriz: {linha_matriz}")
     matriz_lista.append(linha_matriz[:])
     linha_matriz.clear()
+    print(f"Matriz no range: {matriz_lista}")
+# matriz_lista.append(linha_matriz[:])
+# linha_matriz.clear()
 
-    print(f"Matriz no range: {matriz_lista}")   
 print("=-" * 30)
 print(f"Matriz: {matriz_lista}")
 for valor in matriz_lista:
