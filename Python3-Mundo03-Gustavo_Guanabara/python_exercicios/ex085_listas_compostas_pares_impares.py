15d5da23d4cd56d40d1478052b290a11e824002e
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-12 14:49:35 +0200

    Update ex085_listas_compostas_pares_impares.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex085_listas_compostas_pares_impares.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex085_listas_compostas_pares_impares.py
@@ -1,14 +1,15 @@
-lista_completa = [[], []]
-for valores in range(0, 7):
-    numero = int(input("Digite um valor: "))
+# Exercício Python 085: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
 
+lista_completa = [[], []]
+for valores in range(1, 8):
+    numero = int(input(f"Digite o {valores}° valor: "))
     if numero % 2 == 0:
         lista_completa[0].append(numero)
     else:
         lista_completa[1].append(numero)
 lista_completa[0].sort()
 lista_completa[1].sort()
-
+print("=-" * 30)
 print(f"A lista de números completos é: {lista_completa}")
 print(f"Os valores pares digitados foram: {lista_completa[0]}")
 print(f"Os valores ímpares digitados foram: {lista_completa[1]}")
