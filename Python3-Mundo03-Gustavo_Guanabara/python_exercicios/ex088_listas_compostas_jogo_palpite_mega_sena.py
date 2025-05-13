4c9935845a71eb627a62055fc09bb38ab8e499dc
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-13 09:51:23 +0200

    Update ex088_listas_compostas_jogo_palpite_mega_sena.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex088_listas_compostas_jogo_palpite_mega_sena.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex088_listas_compostas_jogo_palpite_mega_sena.py
@@ -5,28 +5,28 @@ from time import sleep
 # print(palpite_mega_sena)
 numeros_mega_sena = []
 lista_mega_sena = []
-contador_jogos = 0
 
 print("-" * 40)
 print("{:^30}".format("JOGOS NA MEGA SENA"))
 print("-" * 40)
 jogos_gerados = int(input("Quantos jogos quer que eu sorteie? "))
+sleep(1)
+print(f"=-" * 20)
+print(f" SORTEANDO {jogos_gerados} JOGOS ")
+print("=-" * 20)
 
 while jogos_gerados > 0:
     for lista in range(0, 6):
         palpite_mega_sena = randint(0, 60)
         numeros_mega_sena.append(palpite_mega_sena)
+    numeros_mega_sena.sort()
     lista_mega_sena.append(numeros_mega_sena[:])
     numeros_mega_sena.clear()
-    contador_jogos += 1
     jogos_gerados -= 1
     # print(f"Lista da mega-sena {lista_mega_sena}")
-sleep(1)
-print(f"=-" * 20)
-print(f" SORTEANDO {jogos_gerados} JOGOS ")
-print("=-" * 20)
+
 for posicao, valor in enumerate(lista_mega_sena):
     sleep(1)
     print(f"Jogo {posicao+1}: {valor} ")
 
-print(f"BOA SORTE")
+print(f"BOA SORTE!!!")
