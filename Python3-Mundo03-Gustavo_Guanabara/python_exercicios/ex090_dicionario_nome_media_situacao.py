d7ad082c6668038a81c65e056d4e1f3ed8b25807
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-14 17:18:40 +0200

    Update ex090_dicionario_nome_media_situacao.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex090_dicionario_nome_media_situacao.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex090_dicionario_nome_media_situacao.py
@@ -1,9 +1,12 @@
 aluno = {}
 aluno["Nome"] = input("Nome: ").strip().capitalize()
-aluno["Média"] = float(input("Média: "))
-if aluno["Média"] >= 6:
+aluno["Média"] = float(input(f"Média de {aluno['Nome']}: "))
+if aluno["Média"] >= 7:
     aluno["Situacao"] = "Aprovado"
+elif 5 <= aluno["Média"] < 7:
+    aluno["Situacao"] = "Recuperação"
 else:
     aluno["Situacao"] = "Reprovado"
+print("=-" * 20)
 for key, value in aluno.items():
     print(f"{key} é igual a {value}")
