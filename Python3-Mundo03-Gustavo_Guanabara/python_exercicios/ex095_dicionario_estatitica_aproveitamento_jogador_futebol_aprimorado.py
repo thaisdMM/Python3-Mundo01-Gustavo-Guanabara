37abe5a352763f72dbafbe1dfc2d03b4bd3b7e9d
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-16 11:02:58 +0200

    Update ex095_dicionario_estatitica_aproveitamento_jogador_futebol_aprimorado.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex095_dicionario_estatitica_aproveitamento_jogador_futebol_aprimorado.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex095_dicionario_estatitica_aproveitamento_jogador_futebol_aprimorado.py
@@ -9,13 +9,13 @@ while True:
         input(f"Quantas partidas {aproveitamento_jogador['nome']} jogou? ")
     )
     for jogos in range(1, partidas_jogadas + 1):
-         gols = int(
+        gols = int(
             input(
                 f"Quantos gols {aproveitamento_jogador['nome']} marcou na partida {jogos}? "
             )
         )
-         lista_gols.append(gols)
-         total_gols += gols
+        lista_gols.append(gols)
+        total_gols += gols
 
     aproveitamento_jogador["gols"] = lista_gols
     aproveitamento_jogador["total"] = total_gols
@@ -28,22 +28,26 @@ while True:
         print("=-" * 30)
         break
     print("-" * 50)
-print(f"lista: {lista_aproveitamento_jogadores}")
+# print(f"lista: {lista_aproveitamento_jogadores}")
 print(f"{'COD':<5} {'NOME':<20} {'GOLS':<25} {'TOTAL':>5}")
 print("-" * 70)
 for posicao, valor in enumerate(lista_aproveitamento_jogadores):
-    print(f"{posicao+1:<5} {valor['nome']:<20} {str(valor['gols']):<25} {valor['total']:>5}")
+    print(
+        f"{posicao+1:<5} {valor['nome']:<20} {str(valor['gols']):<25} {valor['total']:>5}"
+    )
 while True:
-    resposta = int(input("Mostrar os dados de qual jogador? [999 para finalizar o programa] "))
+    resposta = int(
+        input("Mostrar os dados de qual jogador? [999 para finalizar o programa] ")
+    )
     if resposta == 999:
         print("Progama finalizado.")
         break
+    if resposta == 0 or resposta > len(lista_aproveitamento_jogadores):
+        print(f"Jogador inexistente. Verifique o código do jogador.")
     else:
         for posicao, valor in enumerate(lista_aproveitamento_jogadores):
-            if resposta -1 == posicao:
-            #if resposta <= len(lista_aproveitamento_jogadores) -1:
+            if resposta - 1 == posicao:
+                # if resposta <= len(lista_aproveitamento_jogadores) -1:
                 print(f"-- LEVANTAMENTO DO JOGADOR {valor['nome']}: ")
-                for gol in valor["gols"]:
-                    print(f"No jogo {posicao+1} fez {gol}") 
-                # for jogo, gol in enumerate(lista_gols):
-                #     print(f"No jogo {jogo +1} fez {gol} gols.")
+                for partida, gol in enumerate(valor["gols"]):
+                    print(f"No jogo {partida+1} fez {gol}")
