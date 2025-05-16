659e72b645739d8e401c4091bdb81cbd9d826cf1
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-16 14:57:16 +0200

    Update ex094_dicionario_lista_pessoas_cadastro_dados.py
    - correção professor

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex094_dicionario_lista_pessoas_cadastro_dados.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex094_dicionario_lista_pessoas_cadastro_dados.py
@@ -1,9 +1,15 @@
+# Exercício Python 094: Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. No final, mostre: A) Quantas pessoas foram cadastradas B) A média de idade C) Uma lista com as mulheres D) Uma lista de pessoas com idade acima da média
+
 dados_pessoas = list()
 pessoa = dict()
 total_idade = 0
 while True:
-    pessoa["nome"] = input("Nome: ").strip().capitalize()
-    pessoa["sexo"] = input("Sexo: [M/F] ").strip().upper()[0]
+    pessoa["nome"] = input("Nome: ").strip().title()
+    while True:
+        pessoa["sexo"] = input("Sexo: [M/F] ").strip().upper()[0]
+        if pessoa["sexo"] in "MF":
+            break
+        print(f"Resposta inválida. Por favor informe o sexo.")
     pessoa["idade"] = int(input("Idade: "))
     total_idade += pessoa["idade"]
     # print(f"total idade {total_idade}")
@@ -18,7 +24,7 @@ while True:
 print("-" * 30)
 print(f"Ao todo foram cadastradas {len(dados_pessoas)} pessoas.")
 media_idade = total_idade / len(dados_pessoas)
-print(f"A média de idade das pessoas cadastradas foi: {media_idade:.2f}")
+print(f"A média de idade das pessoas cadastradas é: {media_idade:5.2f}")
 print(f"As mulheres cadastradas foram: ", end="")
 for valor in dados_pessoas:
     if valor["sexo"] == "F":
@@ -26,8 +32,9 @@ for valor in dados_pessoas:
 print()
 print(f"Lista de pessoas que estão acima da média da idade:")
 for valor in dados_pessoas:
-    for key, value in valor.items():
-        if valor["idade"] > media_idade:
+    if valor["idade"] >= media_idade:
+        print("     ", end="")
+        for key, value in valor.items():
             print(f"{key} = {value}; ", end="")
-    print()
+        print()
 print("<< ENCERRADO >>")
