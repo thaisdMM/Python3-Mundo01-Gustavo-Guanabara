51078601273ea18eec37206944075ce4a4e2393e
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-20 13:39:30 +0200

    Create ex096_funcao1_msg_linhas_adaptaveis.py

---
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex096_funcao1_msg_linhas_adaptaveis.py
@@ -0,0 +1,9 @@
+def escreva(txt):
+    print("~" * len(txt))
+    print(txt)
+    print("~" * len(txt))
+
+
+escreva(" Thaís Moreira ")
+escreva(" criou uma função escreva ")
+escreva(" com o professor Gustavo Guanabara ")
