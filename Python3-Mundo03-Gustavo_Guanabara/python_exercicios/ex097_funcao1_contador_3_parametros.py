8c9aa0a24e1cd0b95583da0ba6cdecbe80c62530
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-20 15:43:57 +0200

    use abs function to turn number negative in absolute

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex097_funcao1_contador_3_parametros.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex097_funcao1_contador_3_parametros.py
@@ -4,28 +4,28 @@ from time import sleep
 def contador(inicio, fim, passo):
     if passo == 0:
         passo = 1
-        print("=-" * 30)
-        print(f"Contagem de {inicio} até {fim} de {passo} em {passo}")
+        # print("=-" * 30)
+        # print(f"Contagem de {inicio} até {fim} de {passo} em {passo}")
 
     if passo < 0 and inicio > fim:
-        print("=-" * 30)
-        print(f"Contagem de {inicio} até {fim} de {-passo} em {-passo}")
+        passo = passo
+    #     print("=-" * 30)
+    #     print(f"Contagem de {inicio} até {fim} de {-passo} em {-passo}")
     elif inicio > fim:
         passo = -(passo)
-        print("=-" * 30)
-        print(f"Contagem de {inicio} até {fim} de {-passo} em {-passo}")
+        # print("=-" * 30)
+        # print(f"Contagem de {inicio} até {fim} de {-passo} em {-passo}")
 
-    else:
-        print("=-" * 30)
-        print(f"Contagem de {inicio} até {fim} de {passo} em {passo}")
+    # else:
+    # utilizando a função abs() para printar o valor absoluto do número no passo, sem positivo ou negativo, mas no range vai continuar o passo correto, seja ele negativo ou positivo
+    print("=-" * 30)
+    print(f"Contagem de {inicio} até {fim} de {abs(passo)} em {abs(passo)}")
     for i in range(inicio, fim + passo, passo):
-        # sleep(0.5)
         print(f"{i}", end=" ", flush=True)
         sleep(0.5)
     print("FIM!")
     print()
-
-
+    
 contador(1, 10, 1)
 contador(10, 0, 2)
 
