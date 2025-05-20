f27b1f7f7030aa12ad70981ee180959d54f5583c
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-20 13:29:24 +0200

    Create ex096_funcoes1_area_retangulo.py

---
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex096_funcoes1_area_retangulo.py
@@ -0,0 +1,13 @@
+# Exercício Python 096: Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.
+
+
+def area():
+    area_retangulo = largura * cumprimento
+    print(f"A área de um terreno {largura}x{cumprimento} é de {area_retangulo}m.")
+
+
+print("Controle de Terrenos")
+print("-" * 30)
+largura = float(input("LARGURA (m): "))
+cumprimento = float(input("CUMPRIMENTO (m): "))
+area()
