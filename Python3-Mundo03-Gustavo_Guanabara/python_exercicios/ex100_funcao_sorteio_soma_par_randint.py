a98b1018db77f100d934d9ca8320573dacd30885
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-21 11:22:48 +0200

    Update ex100_funcao_sorteio_soma_par_randint.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex100_funcao_sorteio_soma_par_randint.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex100_funcao_sorteio_soma_par_randint.py
@@ -1,28 +1,29 @@
+# Exercício Python 100: Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteia() e somaPar(). A primeira função vai sortear 5 números e vai colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
+
 from random import randint
 from time import sleep
 
-numeros_lista = []
 
-
-def sorteio():
+def sorteio(lista):
     print(f"Sorteadon 5 valores da lista: ", end="")
     for i in range(0, 5):
         numeros = randint(1, 10)
-        numeros_lista.append(numeros)
+        lista.append(numeros)
         print(numeros, end=" ", flush=True)
         sleep(0.6)
     print("PRONTO!")
     print()
 
 
-def somaPar():
+def somaPar(lista):
     soma_pares = 0
-    for valor in numeros_lista:
+    for valor in lista:
         if valor % 2 == 0:
             soma_pares += valor
+    print(f"Somando os valores pares de {lista}, temos: {soma_pares}")
 
-    print(f"Somando os valores pares de {numeros_lista}, temos: {soma_pares}")
 
+numeros_lista = []
 
-sorteio()
-somaPar()
+sorteio(numeros_lista)
+somaPar(numeros_lista)
