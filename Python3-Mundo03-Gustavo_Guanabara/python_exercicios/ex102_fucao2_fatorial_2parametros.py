69f129968f9fb28195474985ada24a57672f9320
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-22 11:24:24 +0200

    Create ex102_fucao2_fatorial_2parametros.py

---
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex102_fucao2_fatorial_2parametros.py
@@ -0,0 +1,25 @@
+# Exercício Python 102: Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
+
+
+def fatorial(numero, show=False):
+    """
+    -> Calcula o fatorial de um número.
+    :param numero: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor de fatorial de um número em param número.
+    """
+    resultado_fatorial = 1
+    for i in range(numero, 0, -1):
+        resultado_fatorial *= i
+        if show == True:
+            print(f"{i}", end="")
+            print(f" x " if i > 1 else " = ", end="")
+            resultado_fatorial
+        else:
+            resultado_fatorial
+
+    return resultado_fatorial
+
+
+print(fatorial(5, True))
+#help(fatorial)
