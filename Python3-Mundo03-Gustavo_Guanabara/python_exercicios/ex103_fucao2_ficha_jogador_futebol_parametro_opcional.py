05fb7c333320a41e2111f2396bb9ede7c56e83ad
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-22 16:12:04 +0200

    Create ex103_fucao2_ficha_jogador_futebol_parametro_opcional.py

---
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex103_fucao2_ficha_jogador_futebol_parametro_opcional.py
@@ -0,0 +1,19 @@
+# Exercício Python 103: Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
+
+
+def ficha(nome="<desconhecido>", gols=0):
+    if nome == "":
+        nome = "<desconhecido>"
+    if gols == "":
+        gols = 0
+
+    print(f"O jogador {nome} fez {int(gols)} no Campeonato.")
+    # print(type(gols))
+
+
+print("-" * 30)
+nome = input("Nome do jogador: ").strip().title()
+gols = input("Número de gols: ")
+ficha(nome, gols)
+# ficha(nome)
+# ficha(gols)
