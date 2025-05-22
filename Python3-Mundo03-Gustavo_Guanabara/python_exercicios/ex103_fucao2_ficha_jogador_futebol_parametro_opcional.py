6f89fa349d3bf013e3d5b269fc41bb78ac33db27
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-22 18:48:21 +0200

    Update ex103_fucao2_ficha_jogador_futebol_parametro_opcional.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex103_fucao2_ficha_jogador_futebol_parametro_opcional.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex103_fucao2_ficha_jogador_futebol_parametro_opcional.py
@@ -1,19 +1,20 @@
-# Exercício Python 103: Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
+# # Exercício Python 103: Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
 
 
 def ficha(nome="<desconhecido>", gols=0):
-    if nome == "":
-        nome = "<desconhecido>"
-    if gols == "":
-        gols = 0
-
-    print(f"O jogador {nome} fez {int(gols)} no Campeonato.")
+    print(f"O jogador {nome} fez {gols} gol(s) no Campeonato.")
     # print(type(gols))
 
 
 print("-" * 30)
 nome = input("Nome do jogador: ").strip().title()
 gols = input("Número de gols: ")
-ficha(nome, gols)
-# ficha(nome)
-# ficha(gols)
+if gols.isnumeric():
+    gols = int(gols)
+else:
+    gols = 0
+if nome == "":
+    ficha(gols=gols)
+    # > Não pega nome do input, vai pegar o nome definido na função e o gol que foi passado aqui no input
+else:
+    ficha(nome, gols)
