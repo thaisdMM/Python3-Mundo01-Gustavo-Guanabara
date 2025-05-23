18af99a1ef497212e13f621e4d2a333d2c9f6f04
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-23 10:11:49 +0200

    Update ex104_fucao2_validando_entradas_leia_int.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex104_fucao2_validando_entradas_leia_int.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex104_fucao2_validando_entradas_leia_int.py
@@ -2,16 +2,39 @@
 
 
 def leiaInt(msg):
+    print("-" * 30)
     while True:
         print(msg, end="")
         entrada_usuario = input().strip()
         if entrada_usuario.isnumeric():
             break
-        if entrada_usuario[0] == "-" and entrada_usuario[1:].isnumeric():
-            break
-        print("ERRO! Digite um número inteiro válido.")
+        if len(entrada_usuario) > 0:
+            if entrada_usuario[0] == "-" and entrada_usuario[1:].isnumeric():
+                break
+        print("\033[0;31mERRO! Digite um número inteiro válido.\033[m")
     entrada_usuario = int(entrada_usuario)
     return entrada_usuario
+
+
+numero = leiaInt("Digite um número: ")
+print(f"Você acabou de digitar o número {numero}")
+
+# CORREÇÃO DO PROFESSOR:
+
+
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        numero = input(msg)
+        if numero.isnumeric():
+            valor = int(numero)
+            ok = True
+        else:
+            print("\033[0;31mERRO! Digite um número inteiro válido\033[m")
+        if ok:
+            break
+    return valor
 
 
 numero = leiaInt("Digite um número: ")
