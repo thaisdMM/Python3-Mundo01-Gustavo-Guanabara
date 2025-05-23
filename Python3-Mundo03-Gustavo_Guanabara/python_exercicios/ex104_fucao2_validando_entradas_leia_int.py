2462fa852590793e6739e45651bdbd1f513dc15a
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-05-23 09:34:28 +0200

    Update ex104_fucao2_validando_entradas_leia_int.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex104_fucao2_validando_entradas_leia_int.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex104_fucao2_validando_entradas_leia_int.py
@@ -1,5 +1,6 @@
 # Exercício Python 104: Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante ‘a função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico. Ex: n = leiaInt(‘Digite um n: ‘)
 
+
 def leiaInt(msg):
     while True:
         print(msg, end="")
@@ -8,28 +9,10 @@ def leiaInt(msg):
             break
         if entrada_usuario[0] == "-" and entrada_usuario[1:].isnumeric():
             break
-        # # elif entrada_usuario.isalpha():
-        # for valor in entrada_usuario:
-        #     print(f"valor {valor}")
-        #     if valor.isalpha():
-        #         print("ERRO!")
-            
-        # else:
-        #     entrada_usuario = int(entrada_usuario)
-        #     if entrada_usuario < 0:
-        #         print(f"<0 {entrada_usuario}")
-        #         break
-        
         print("ERRO! Digite um número inteiro válido.")
-        # print(msg, end="")
-        # msg = input()
-        # #if msg.isnumeric() == False:
-           # print("ERRO! Digite um número inteiro válido.")
-        
     entrada_usuario = int(entrada_usuario)
     return entrada_usuario
-    # else:
-        #     print("ERRO! Digite um número inteiro válido.")
+
 
 numero = leiaInt("Digite um número: ")
 print(f"Você acabou de digitar o número {numero}")
