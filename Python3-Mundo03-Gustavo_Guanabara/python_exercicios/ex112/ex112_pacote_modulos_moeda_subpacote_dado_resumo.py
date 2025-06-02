82d09c9f8f250842d131b628fccbd4da0f4e952d
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-02 15:32:01 +0200

    rename files and completed dado.py

---
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex112/ex112_pacote_modulos_moeda_subpacote_dado_resumo.py
@@ -0,0 +1,9 @@
+# # Exercício Python 112: Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado. Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função imputa(), mas com uma validação de dados para aceitar apenas valores que seja monetários
+
+from pacote_utilidades.subpacote_dado import dado
+from pacote_utilidades.subpacote_moeda import moeda
+
+preco = dado.leiaDinheiro("Digite o preco: R$")
+moeda.resumo(preco, 80, 35)
+# moeda.resumo(preco, 12, 20)
+# moeda.resumo(preco)
--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex112/pacote_utilidades/subpacote_dado/dado.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex112/pacote_utilidades/subpacote_dado/dado.py
@@ -1,77 +1,51 @@
-# Exercício Python 112: Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado. Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função imputa(), mas com uma validação de dados para aceitar apenas valores que seja monetários
-
-
 def leiaDinheiro(msg):
     while True:
         eh_monetario = False
         valor = 0
         numero = input(msg)
+        if not numero:
+            print(f"\033[0;31mERRO! Não foi digitado nada.\033[m")
+            continue
         if numero.isnumeric():
-            valor = int(numero)
+            valor = float(numero)
             eh_monetario = True
         else:
-            encontrou_numero = 0
             encontrou_caractere = 0
+            virgula_caractere = ""
             if numero[0].isdigit() == False:
                 eh_monetario = False
-                print(f"ERRO! Caractere primeira entrada não é numérica")
+                print(
+                    f"\033[0;31mERRO! Caractere '{numero}' da primeira entrada não é monetária.\033[m"
+                )
                 continue
             for caractere in numero:
-                # if caractere[0]:
-                #     if caractere.isnumeric():
-                #         encontrou_numero +=1
-                #     else:
-                #         eh_monetario = False
-                #         print(f"ERRO! Caractere primeira entrada não é numérica")
-                #         break
-                # else:
                 if caractere.isnumeric():
                     continue
                 if caractere not in ",.":
                     eh_monetario = False
-                    print(f"ERRO! Caractere diferente de ., .")
+                    print(
+                        f"\033[0;31mERRO! Caractere '{caractere}' diferente dos separadores monetários [.,]\033[m"
+                    )
                     break
                 else:
                     if caractere in ".":
-                        print(caractere)
                         encontrou_caractere += 1
-                    if caractere in ',':
+                    if caractere in ",":
                         encontrou_caractere += 1
+                        virgula_caractere += caractere
                 if encontrou_caractere > 1:
-                    print(f"ERRO! Mais caracteres.")
+                    print(
+                        f"\033[0;31mERRO!'{numero}' Mais separadores decimais do que permitido.\033[m"
+                    )
                     eh_monetario = False
                     break
-                if encontrou_caractere == 1:
-
-                    valor = numero
-                    eh_monetario = True
-            
-                
-
-                # if caractere != "," and caractere != ".":
-                #     eh_monetario = False
-                #     print(f"ERRO! Caractereo diferente.")
-                #     break
-                # if caractere == ',' or caractere == '.':
-                
-                #     encontrou_caractere += 1
-                # if encontrou_caractere > 1:
-                #     print(f"ERRO! Mais caractereos.")
-                #     eh_monetario = False
-                #     break
-                # if encontrou_caractere == 1:
-
-                #     valor = numero
-                #     eh_monetario = True
-                # else:
-                #     not eh_monetario
-                #     break
-
-
+            if encontrou_caractere == 1:
+                eh_monetario = True
+                if len(virgula_caractere) == 1:
+                    troca_virgula = numero.replace(",", ".")
+                    valor = float(troca_virgula)
+                else:
+                    valor = float(numero)
         if eh_monetario:
             break
     return valor
-
-
-numero = leiaDinheiro("digite um valor monetário: ")
-print(f"O valor digitado foi {numero}")
