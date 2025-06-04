2059d683a41a1579175c7f1df39d3ed651897c21
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-04 15:40:24 +0200

    Update funcoes_menu_cadastro_pessoas.py

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex115/funcoes_menu_cadastro_pessoas.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex115/funcoes_menu_cadastro_pessoas.py
@@ -23,18 +23,42 @@ def menu():
     )
 
 
-def cadastrar_pessoa(lista, nome, idade):
-    lista.append([nome, idade])
+def cadastrar_pessoa(nome, idade):
+    file_path = "lista_pessoas_idade_sistema_ex115.txt"
+    with open(file_path, "a") as arquivo:
+        arquivo.write(
+            f"{nome}\n",
+        )
+        arquivo.write(
+            f"{idade} anos\n",
+        )
 
 
-def ver_pessoas_cadastradas(lista):
-    for pessoa in lista:
-        print(f"\t{pessoa}", end="")
-    print()
+def ver_pessoas_cadastradas():
+    file_path = "lista_pessoas_idade_sistema_ex115.txt"
+    try:
+        with open(file_path, "r") as arquivo:
+            conteudo = arquivo.read()
+    except FileNotFoundError:
+        print("O arquivo não foi encontrado. Verifique se o arquivo existe.")
+    except PermissionError:
+        print("Você não tem permissão para ler esse arquivo.")
+    else:
+        for line in conteudo:
+            print(line, end="")
+        # print(conteudo)
+        # for line in conteudo:
+        #     line.strip()
+        #     line.split(';')
+        #     print(f'{line}', end="")
+
+    # for pessoa in lista:
+    #     print(f"\t{pessoa}", end="")
+    # print()
 
 
 lista_pessoas = []
-
+arquivo = "lista_pessoas_idade_sistema_ex115.txt"
 
 while True:
     menu()
@@ -46,13 +70,13 @@ while True:
 
     if opcao == 1:
         titulo("LISTA DE PESOAS CADASTRADAS")
-        ver_pessoas_cadastradas(lista_pessoas)
+        ver_pessoas_cadastradas()
 
     if opcao == 2:
         titulo("NOVO CADASTRO")
-        nome = input("Nome: ")
-        idade = input("Idade: ")
-        cadastrar_pessoa(lista_pessoas, nome, idade)
+        nome = input("Nome: ").strip().title()
+        idade = input("Idade: ").strip()
+        cadastrar_pessoa(nome, idade)
 
     if opcao == 3:
         print("Programa finalizado!")
