c3cf060e35dad2ad08e98f880044fc6ec2225bb5
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-06-04 18:05:50 +0200

    main programm started

--- Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex115/sistema.py
+++ Python3-Mundo03-Gustavo_Guanabara/python_exercicios/ex115/sistema.py
@@ -1,3 +1,17 @@
 from library.interface import interface_sistema
+from time import sleep
 
-interface_sistema.menu(["opcao1", "opcao2", "opcao3"])
+
+while True:
+    resposta = interface_sistema.menu(["Ver Pessoas Cadastradas", "Cadastrar Nova Pessoa", "Sair do Sistema"])
+    if resposta == 1:
+        interface_sistema.titulo("LISTA DE PESOAS CADASTRADAS")
+    elif resposta == 2:
+        interface_sistema.titulo("NOVO CADASTRO")
+
+    elif resposta == 3:
+        interface_sistema.titulo("Saindo do Sistema... Até logo!")
+        break
+    else:
+        print(f"\033[31mERRO! Opção '{resposta}' inexistente no menu.\033[m")
+    sleep(1)
